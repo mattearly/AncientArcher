a8bfe9afa705b45841ac8008fe28652f7f1b3798
Matthew Early <matthewjearly@gmail.com>
2018-11-24 18:46:30 -0500

    updates

--- AncientArcher/src/main.cpp
+++ AncientArcher/src/main.cpp
@@ -12,6 +12,11 @@ float vertices[] = {
 	 0.0f,  0.5f, 0.0f
 };
 
+unsigned int indices[] = {
+	0, 1, 3,
+	1, 2, 3
+};
+
 const char * vertexShaderSource = "#version 330 core\n"
 "layout(location = 0) in vec3 aPos;\n"
 "void main() {\n"
@@ -48,16 +53,21 @@ int main() {
 		return -1;
 	}
 
-	/* set up an area to store a vertex array */
+	/* set up an area to store a vertex data */
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-
-	/* send triangle vertices to graphics card */
 	unsigned int VBO;
 	glGenBuffers(1, &VBO);
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+
+	/* send triangle vertices to graphics card */
+	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	/* compile vertex shader */
 	unsigned int vertexShader;
@@ -108,26 +118,32 @@ int main() {
 		return -1;
 	}
 
-	/* use the shaderProgram we just set up */
-	glUseProgram(shaderProgram);
-
-	/* shaderProgram handles the data from these so we don't need them anymore */
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
-
 	/* set which vertex attribute to use for upcoming operations */
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	/* main program loop */
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
 		glClearColor(0.2f, 0.3f, 0.2f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		/* use the shaderProgram we just set up */
+		glUseProgram(shaderProgram);
+		
+		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
 		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);  // we don't really need to unbind it every time
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	/* we don't need them anymore */
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
 
 	glfwTerminate();
 
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Ancient Archer (TM)
+
+_A game by Matthew Early_
