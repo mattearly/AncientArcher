b79e8428c40798613fcb53bdb0a28a2a308652eb
Matthew Early <matthewjearly@gmail.com>
2018-12-03 13:24:55 -0500

    include updates - include req now as well

--- AncientArcher/src/main.cpp
+++ AncientArcher/src/main.cpp
@@ -1,12 +1,13 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
-#include <string>
-#include <iostream>
-#include "../req/stb_image.h"
-#include "../req/Shader.h"
+#include <stb_image.h>
+
+#include <Shader.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+
+#include <iostream>
 
 void processInput(GLFWwindow* window);
 
