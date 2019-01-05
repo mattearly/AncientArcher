49b4969e7a16c7620856d2ca9cd9c7010a7b5985
Matthew Early <matthewjearly@gmail.com>
2019-01-05 16:21:53 -0500

    cleanup

--- AncientArcher/src/lighting/Lighting.cpp
+++ AncientArcher/src/lighting/Lighting.cpp
@@ -9,19 +9,13 @@
 
 extern Shader shader;
 
-Lighting::Lighting() {
-
-}
+Lighting::Lighting() {}
 
 Lighting::~Lighting() {}
 
-//void Lighting::draw() {
-//
-//}
-
 void Lighting::setConstantLight() {
 
-  //// directional light
+  // directional light
   shader.setVec3("dirLight.direction", -0.2f, -1.0f, -0.3f);
   shader.setVec3("dirLight.ambient", 0.05f, 0.05f, 0.05f);
   //shader.setVec3("dirLight.diffuse", 0.4f, 0.4f, 0.4f);
