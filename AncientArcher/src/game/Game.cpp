e3897c80753bad8cc4d378ae4aad346125610f16
Matthew Early <matthewjearly@gmail.com>
2019-02-14 19:18:31 -0500

    added 16 more textures (32 max now)

--- AncientArcher/src/game/Game.cpp
+++ AncientArcher/src/game/Game.cpp
@@ -25,23 +25,39 @@ Game::Game() {
 
 	player = new Player();   //default character
 
-	/* load 16 textures */
+	/* load 32 textures */
 	texBank.loadTexture("../AncientArcher/res/texture/00-pixelized_grass.png");
 	texBank.loadTexture("../AncientArcher/res/texture/01.png");
 	texBank.loadTexture("../AncientArcher/res/texture/02-pixelized_wood.png");
-	texBank.loadTexture("../AncientArcher/res/texture/03-pixelized_shrub.png");
+	texBank.loadTexture("../AncientArcher/res/texture/03-footsteps.png");
 	texBank.loadTexture("../AncientArcher/res/texture/04-pixelized_portal.png");
 	texBank.loadTexture("../AncientArcher/res/texture/05-pixelized_darkstone.png");
 	texBank.loadTexture("../AncientArcher/res/texture/06-pixelized_water_droplet.png");
 	texBank.loadTexture("../AncientArcher/res/texture/07-pixelized_gravel.png");
 	texBank.loadTexture("../AncientArcher/res/texture/08-pixelized_water.png");
-	texBank.loadTexture("../AncientArcher/res/texture/09-pixelized_player.png");
+	texBank.loadTexture("../AncientArcher/res/texture/09-bricks_light.png");
 	texBank.loadTexture("../AncientArcher/res/texture/10-pixelized_mud.png");
 	texBank.loadTexture("../AncientArcher/res/texture/11-pixelized_darkwood.png");
 	texBank.loadTexture("../AncientArcher/res/texture/12-pickup_speedboost.png");
 	texBank.loadTexture("../AncientArcher/res/texture/13-pixelized_snow.png");
 	texBank.loadTexture("../AncientArcher/res/texture/14-maze_metal.png");
 	texBank.loadTexture("../AncientArcher/res/texture/15-pickup_attackboost.png");
+	texBank.loadTexture("../AncientArcher/res/texture/16-cracked_rock.png");
+	texBank.loadTexture("../AncientArcher/res/texture/17-stone_texture.png");
+	texBank.loadTexture("../AncientArcher/res/texture/18-black_schist_rock.png");
+	texBank.loadTexture("../AncientArcher/res/texture/19-red_brick.png");
+	texBank.loadTexture("../AncientArcher/res/texture/20-blue_paint.png");
+	texBank.loadTexture("../AncientArcher/res/texture/21-cracked_mud.png");
+	texBank.loadTexture("../AncientArcher/res/texture/22-fern_leaves.png");
+	texBank.loadTexture("../AncientArcher/res/texture/23-cracked_mud_light.png");
+	texBank.loadTexture("../AncientArcher/res/texture/24-hardwood_floor.png");
+	texBank.loadTexture("../AncientArcher/res/texture/25-leather_dark.png");
+	texBank.loadTexture("../AncientArcher/res/texture/26-rock_with_holes.png");
+	texBank.loadTexture("../AncientArcher/res/texture/27-skull_mud.png");
+	texBank.loadTexture("../AncientArcher/res/texture/28-plastic_grip.png");
+	texBank.loadTexture("../AncientArcher/res/texture/29-colorful_window.png");
+	texBank.loadTexture("../AncientArcher/res/texture/30-water_refractions.png");
+	texBank.loadTexture("../AncientArcher/res/texture/31-water_top.png");
 
 	diffuseTex.loadDiffuseTexture("../AncientArcher/res/specular/none.png");
 
@@ -87,7 +103,7 @@ Game::Game() {
 			// SIZE
 			glm::vec3(width, 0.5f, width),
 			// TEXTURE ID
-			i,
+			mearly::NTKR(0, 31),
 			// COLLISION?
 			false
 		);
@@ -103,7 +119,7 @@ Game::Game() {
 			// SIZE
 			glm::vec3(width, 0.5f, width),
 			// TEXTURE ID
-			i + 2,
+			mearly::NTKR(0, 31),
 			// COLLISION?
 			false
 		);
@@ -119,7 +135,7 @@ Game::Game() {
 			// SIZE
 			glm::vec3(width, 1.0f, width),
 			// TEXTURE ID
-			i + 3,
+			mearly::NTKR(0, 31),
 			// COLLISION?
 			false
 		);
@@ -132,7 +148,7 @@ Game::Game() {
 			ENTITYTYPE::SQUARE, 
 			glm::vec3(mearly::NTRK(3.f, 100.f), 1.0f, mearly::NTRK(3.f, 120.f)), 
 			glm::vec3(2.f, 2.f, 2.f), 
-			mearly::NTKR(1, 15), 
+			mearly::NTKR(0, 31), 
 			true
 		);
 		entities.push_back(e);
