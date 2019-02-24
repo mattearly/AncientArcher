1d0ba01030f9a1a44e3d2e9444a2081548b15997
Matthew Early <matthewjearly@gmail.com>
2019-02-23 22:51:27 -0500

    remove collision class, just using collider on the entity and trying out collision in utilities or physics

--- AncientArcher/src/game/Game.h
+++ AncientArcher/src/game/Game.h
@@ -2,18 +2,15 @@
 #include "../player/Player.h"
 #include "../renderer/Renderer.h"
 #include "../models/PrimativeManager.h"
-#include "../models/Collision.h"
 
 class Game {
 public:
 
   Player *player;
-  Collision collision;
   PrimativeManager primativeManager;
   Renderer renderer;
 
   Game();
-  ~Game();
 
   void mainLoop();
 
--- AncientArcher/src/models/Collision.cpp
+++
@@ -1,6 +0,0 @@
-#include "Collision.h"
-
-void Collision::processPointCollision(Player * player, const float dtime, const glm::vec3 original_loc, const glm::vec3 intended_loc)
-{
-  
-}
