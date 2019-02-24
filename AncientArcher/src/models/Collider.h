b8f0547a328788d9c6b303887f7c0be754e55483
Matthew Early <matthewjearly@gmail.com>
2019-02-24 10:12:41 -0500

    using boundry checker from util/mearly.h

--- AncientArcher/src/models/Collider.h
+++ AncientArcher/src/models/Collider.h
@@ -1,15 +1,13 @@
 #pragma once
 #include <glm/glm.hpp>
-struct Impasse {
-  float location[3];
-  float size[3];
-};
+#include "../util/mearly.h"
+
 class Collider
 {
 private:
   const float COLLISION_EXPANDER = .3f;
 public:
-  Impasse impasse;
+  mearly::Impasse impasse;
   Collider(glm::vec3 loc, glm::vec3 sz);
 };
 
