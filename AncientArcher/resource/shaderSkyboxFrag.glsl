98fb43f493aa317e73249bc17fa436b6f37b536c
Matthew Early <matthewjearly@gmail.com>
2019-05-05 01:41:46 -0400

    updates

--- AncientArcher/resource/shaderSkyboxFrag.glsl
+++ AncientArcher/resource/shaderSkyboxFrag.glsl
@@ -1,11 +1,10 @@
-#version 330
+#version 400
 
 in vec3 textureCoords;
 out vec4 out_Color;
 
 uniform samplerCube cubeMap;
 
-void main(void)
-{
-  out_Color = texture(cubeMap, textureCoords;) 
+void main(void){
+    out_Color = texture(cubeMap, textureCoords);
 }
--- AncientArcher/resource/shaderSkyboxVert.glsl
+++ AncientArcher/resource/shaderSkyboxVert.glsl
@@ -1,4 +1,4 @@
-#version 330
+#version 400
 
 in vec3 position;
 out vec3 textureCoords;
@@ -6,8 +6,9 @@ out vec3 textureCoords;
 uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
 
-void main(void)
-{
-  gl_Position = projectionMatrix + viewMatrix + vec4(positiion, 1.0);
-  textureCoords = position;
+void main(void){
+	
+	gl_Position = projectionMatrix * viewMatrix * vec4(position, 1.0); 
+	textureCoords = position;
+	
 }
