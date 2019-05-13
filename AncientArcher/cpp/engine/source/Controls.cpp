1b82227f828c2b8fde8c536ae41ea1e653da99d6
Matthew Early <matthewjearly@gmail.com>
2019-05-12 23:38:40 -0400

    Add in type switches to functions

--- AncientArcher/cpp/engine/source/Controls.cpp
+++ AncientArcher/cpp/engine/source/Controls.cpp
@@ -18,131 +18,149 @@ Controls::Controls() {
   lastY = 0.f;
 }
 
-void Controls::mouseMovement(float xpos, float ypos, Camera *cam, ) {
+void Controls::mouseMovement(float xpos, float ypos, Camera *cam, ControlType type) {
 
-  /// This block is first person controls
-  /// Note: cursor visibility can be toggled in the Display class, it should be OFF for these controls
-  ///
-  if (firstMouse) {
+
+  switch (type) {
+  case ControlType::FPS:
+    /// This block is first person controls
+    /// Note: cursor visibility can be toggled in the Display class, it should be OFF for these controls
+    ///
+    if (firstMouse) {
+      lastX = xpos;
+      lastY = ypos;
+      firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
     lastX = xpos;
     lastY = ypos;
-    firstMouse = false;
+
+    xoffset *= mouseSensitivity;
+    yoffset *= mouseSensitivity;
+
+    cam->Yaw += xoffset;
+    cam->Pitch += yoffset;
+
+    if (cam->Pitch > 89.0f) {
+      cam->Pitch = 89.0f;
+    }
+    else if (cam->Pitch < -89.0f) {
+      cam->Pitch = -89.0f;
+    }
+
+    cam->updateCameraVectors();
+    break;
+  case ControlType::TOP_DOWN:
+    ///   do nothing for now
+    ///
+    ///
+
+    break;
+
+
   }
 
-  float xoffset = xpos - lastX;
-  float yoffset = lastY - ypos;
-  lastX = xpos;
-  lastY = ypos;
-
-  xoffset *= mouseSensitivity;
-  yoffset *= mouseSensitivity;
-
-  cam->Yaw += xoffset;
-  cam->Pitch += yoffset;
-
-  if (cam->Pitch > 89.0f) {
-    cam->Pitch = 89.0f;
-  }
-  else if (cam->Pitch < -89.0f) {
-    cam->Pitch = -89.0f;
-  }
-
-  cam->updateCameraVectors();
-  
-  ///
-  ///
-  ///
 
 
 }
 
-void Controls::playerKeyboardInput()
+void Controls::playerKeyboardInput(ControlType type)
 {
-  if (glfwGetMouseButton(display.window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
-     // left click
-  }
-
-  if (glfwGetKey(display.window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-    glfwSetWindowShouldClose(display.window, true);
-  }
-
-  if (glfwGetKey(display.window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
-    if (movedir.onGround) {
-      movedir.boost = true;
+  switch (type)
+  {
+  case ControlType::FPS:
+    if (glfwGetMouseButton(display.window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+      // left click
     }
-  }
 
-  if (glfwGetKey(display.window, GLFW_KEY_W) == GLFW_PRESS) {
-    if (movedir.onGround) {
-      movedir.back = false;
-      movedir.forward = true;
+    if (glfwGetKey(display.window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+      glfwSetWindowShouldClose(display.window, true);
     }
-  }
 
-  if (glfwGetKey(display.window, GLFW_KEY_S) == GLFW_PRESS) {
-    if (movedir.onGround) {
+    if (glfwGetKey(display.window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
+      if (movedir.onGround) {
+        movedir.boost = true;
+      }
+    }
+
+    if (glfwGetKey(display.window, GLFW_KEY_W) == GLFW_PRESS) {
+      if (movedir.onGround) {
+        movedir.back = false;
+        movedir.forward = true;
+      }
+    }
+
+    if (glfwGetKey(display.window, GLFW_KEY_S) == GLFW_PRESS) {
+      if (movedir.onGround) {
+        movedir.forward = false;
+        movedir.back = true;
+
+      }
+    }
+
+    if (glfwGetKey(display.window, GLFW_KEY_A) == GLFW_PRESS) {
+      if (movedir.onGround) {
+        movedir.right = false;
+        movedir.left = true;
+
+      }
+    }
+
+    if (glfwGetKey(display.window, GLFW_KEY_D) == GLFW_PRESS) {
+      if (movedir.onGround) {
+        movedir.left = false;
+        movedir.right = true;
+      }
+    }
+
+    if (glfwGetKey(display.window, GLFW_KEY_M) == GLFW_PRESS) {
+      toggleAmbientWindyNight();
+    }
+
+    if (glfwGetKey(display.window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+      if (movedir.onGround && movedir.canJumpAgain) {  //can jump again is to make the spacebar spam by holding it down not work
+        movedir.jumped = true;
+        movedir.canJumpAgain = false;
+      }
+    }
+
+    if (glfwGetKey(display.window, GLFW_KEY_0) == GLFW_PRESS) {
+    }
+
+    if (glfwGetKey(display.window, GLFW_KEY_1) == GLFW_PRESS) {
+    }
+
+    if (glfwGetKey(display.window, GLFW_KEY_2) == GLFW_PRESS) {
+    }
+
+    if (glfwGetKey(display.window, GLFW_KEY_W) == GLFW_RELEASE) {
       movedir.forward = false;
-      movedir.back = true;
-
     }
-  }
 
-  if (glfwGetKey(display.window, GLFW_KEY_A) == GLFW_PRESS) {
-    if (movedir.onGround) {
-      movedir.right = false;
-      movedir.left = true;
-
+    if (glfwGetKey(display.window, GLFW_KEY_S) == GLFW_RELEASE) {
+      movedir.back = false;
     }
-  }
 
-  if (glfwGetKey(display.window, GLFW_KEY_D) == GLFW_PRESS) {
-    if (movedir.onGround) {
+    if (glfwGetKey(display.window, GLFW_KEY_A) == GLFW_RELEASE) {
       movedir.left = false;
-      movedir.right = true;
     }
-  }
 
-  if (glfwGetKey(display.window, GLFW_KEY_M) == GLFW_PRESS) {
-    toggleAmbientWindyNight();
-  }
-
-  if (glfwGetKey(display.window, GLFW_KEY_SPACE) == GLFW_PRESS) {
-    if (movedir.onGround && movedir.canJumpAgain) {  //can jump again is to make the spacebar spam by holding it down not work
-      movedir.jumped = true;
-      movedir.canJumpAgain = false;
+    if (glfwGetKey(display.window, GLFW_KEY_D) == GLFW_RELEASE) {
+      movedir.right = false;
     }
-  }
 
-  if (glfwGetKey(display.window, GLFW_KEY_0) == GLFW_PRESS) {
-  }
+    if (glfwGetKey(display.window, GLFW_KEY_LEFT_SHIFT) == GLFW_RELEASE) {
+      movedir.boost = false;
+    }
 
-  if (glfwGetKey(display.window, GLFW_KEY_1) == GLFW_PRESS) {
+    if (glfwGetKey(display.window, GLFW_KEY_SPACE) == GLFW_RELEASE) {
+      movedir.canJumpAgain = true;
+    }
+    break;
+  case ControlType::TOP_DOWN:
+    break;
   }
-
-  if (glfwGetKey(display.window, GLFW_KEY_2) == GLFW_PRESS) {
-  }
-
-  if (glfwGetKey(display.window, GLFW_KEY_W) == GLFW_RELEASE) {
-    movedir.forward = false;
-  }
-
-  if (glfwGetKey(display.window, GLFW_KEY_S) == GLFW_RELEASE) {
-    movedir.back = false;
-  }
-
-  if (glfwGetKey(display.window, GLFW_KEY_A) == GLFW_RELEASE) {
-    movedir.left = false;
-  }
-
-  if (glfwGetKey(display.window, GLFW_KEY_D) == GLFW_RELEASE) {
-    movedir.right = false;
-  }
-
-  if (glfwGetKey(display.window, GLFW_KEY_LEFT_SHIFT) == GLFW_RELEASE) {
-    movedir.boost = false;
-  }
-
-  if (glfwGetKey(display.window, GLFW_KEY_SPACE) == GLFW_RELEASE) {
-    movedir.canJumpAgain = true;
-  }
+  
 }
