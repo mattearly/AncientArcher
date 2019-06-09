6e482a4a074f10c5bdd87a2a199f771852fbdd9f
Eric Bitikofer <e.bitikofer@gmail.com>
2019-06-09 03:24:32 -0400

    Fixed syntax

--- AncientArcher/cpp/pckgs/firstPersonPlayer/FirstPersonPlayer.cpp
+++ AncientArcher/cpp/pckgs/firstPersonPlayer/FirstPersonPlayer.cpp
@@ -49,128 +49,6 @@ FirstPersonPlayer::FirstPersonPlayer(float leg_power)
   jumpTimer = 0.0f;
 }
 
-void FirstPersonPlayer::update(float deltaTime)
-{
-  // KEYBOARD
-  g_controls.fppKeyboardIn(this);
-
-  // MOVEMENT 
-  float velocity;
-  moves.positionChanged = true;
-  model.get()->getEntityPtr()->syncLocation();
-
-
-
-
-
-  if (!moves.onGround)
-  {
-    if (moves.falling)
-    {
-      model.get()->getEntityPtr()->moveBy(glm::vec3(0, /* fall speed */ -0.1f * deltaTime, 0));
-    }
-  }
-  else // moves.onGround
-  {
-
-    // --- SPEED --- // 
-    velocity = getRunSpeed();                         // get player's statebased movement
-    if (moves.forward && (moves.left || moves.right))
-    {
-      velocity = getRunSpeed() / 2;                   // LEFT+FORWARD OR RIGHT+FORWARD = HALF SPEED
-    }
-    if (moves.boost && moves.forward)
-    {
-      velocity *= 2.0;                                // BOOSTING?
-    }
-    velocity *= deltaTime;                            // Final Result calculated with deltaTime.
-
-
-    // --- DIRECTION --- //
-
-  // locks moving foward and backwards to the x and z axii. 
-  // Note: you can use the camera.Front instead of movefront to do a fly type thing 
-  // while the Y is unlocked or you are jumping
-    if (moves.back || moves.forward) {
-      glm::vec3 moveFront = glm::vec3((float)g_camera.getFront()->x, 0.0f, (float)g_camera.getFront()->z); //get looking direction from the cam;
-      if (moves.forward)     model.get()->getEntityPtr()->moveBy(moveFront * velocity);
-      else if (moves.back)   model.get()->getEntityPtr()->moveBy(-moveFront * velocity);
-    }
-    if (moves.right) model.get()->getEntityPtr()->moveBy(*g_camera.getRight() * velocity);
-    else if (moves.left) model.get()->getEntityPtr()->moveBy(-(*g_camera.getRight()) * velocity);
-
-    //// ------------ JUMP SYSTEM -------------- //
-    //// * 3 phase system
-
-    //// PHASE 1: Liftoff
-    //// Liftoff is where the jump is triggered and sounds/animations are played.
-    //if (movedir.jumped) { // if a jump is triggered
-    //  movedir.onGround = false; // the player leaves the ground
-    //  movedir.jumped = false; // the jump is untriggered
-    //  //liftoff animation here
-    //  playgruntsound();
-    //}
-
-    //// PHASE 2: Arc
-    //// Rising and falling are now combined into one function which is
-    //// just a quadratic equation based on the length of the jump action
-    //else if (!movedir.onGround) { // && !movedir.falling) { 
-    //  jumpTimer += deltaTime; // Elapsed time of the player being 'in air'
-    //  float jumpMod = 0.5f / BASE_PLAYER_WEIGHT; // Modifier based on the user weight in 'kg' (probably needs rework)
-    //  float jumpPos = 8.0f; // Initial Y intercept of jump, I think.. not sure why its 8.0f ('c' term in equation below)
-    //  float jumpVel = getRisingSpeed() * jumpTimer; // Velocity of jump ('bt' term in equation below) // RISING SPEED CALC: jump speed based on LegPower Player Stat
-    //  float jumpAccel = -32.1522f * pow(jumpTimer, 2.f); // Accelaration of jump due top gravity in 'feet' (at^2 term in equation below)
-    //              // modifier * (	  c	   +	bt	 +	 at^2	) 
-    //  playerIntendedLocation.y += jumpMod * (jumpPos + jumpVel + jumpAccel); // Parabolic equation based on time
-    //  //std::cout << previousPlayerLocation.y - playerIntendedLocation.y << "\n";
-    //  //if (playerIntendedLocation.y > getJumpHeight() + movedir.lastOnGroundHeight) // MAX HEIGHT CALC: jump height based on LegPower Player Stat
-    //  if (previousPlayerLocation.y - playerIntendedLocation.y > 0) {
-    //    movedir.falling = true;
-    //    // falling animation here
-    //  }
-    //  else if (previousPlayerLocation.y - playerIntendedLocation.y < 0) {
-    //    // rising animation here
-    //  }
-  }
-
-  //// PHASE 3: Landing
-  //// Landing will now just be triggered by the general collision
-  //// checking but it may make sense to put animations or sounds
-  //// here for landing 
-  //if (movedir.positionChanged) { // If the player moves
-  //  for (auto const& e : *entities) { // For every entity
-  //    bool didCollide = checkBoundCollisionWithEntity(e, collider, playerIntendedLocation); // check if the player collided
-  //    if (didCollide) { // if it did collide
-  //      if (!movedir.onGround) { // if player is off the ground
-  //        playerIntendedLocation = *camera.getPosition(); // move the collision checker to the camera position
-  //        movedir.falling = true; // player is falling
-  //      }
-  //      else { // if player is on the ground
-  //        movedir.positionChanged = false; // the position doesn't change
-  //      }
-  //      float ytop = e.collider->impasse.loc[1] + e.collider->impasse.sz[1] / 2; // top of the entity
-  //      if (playerIntendedLocation.y > ytop) { // if the player Y locations is higher than the top of the entity
-  //        movedir.falling = false; // player isn't falling
-  //        movedir.onGround = true; // player is on groung
-  //        jumpTimer = 0.0f; // reset the jump timer
-  //        movedir.lastOnGroundHeight = camera.getPosition()->y; // the last ground height is the current position
-  //        // landing animation here
-  //        playlandingsound();
-  //      }
-  //    }
-  //  }
-  //}
-
-
-
-}
-
-
-
-
-
-
-
 /**
  * Returns the calculated Unboosted Movement Speed of the player. Based on player stats.
  */
@@ -235,8 +113,6 @@ void FirstPersonPlayer::increaseLegPower(float add) {
   }
 }
 
-<<<<<<< Updated upstream
-=======
 void FirstPersonPlayer::update(float deltaTime)
 {
   // KEYBOARD
@@ -270,17 +146,3 @@ void FirstPersonPlayer::update(float deltaTime)
 
 
 }
-
-float FirstPersonPlayer::getRunSpeed() const {
-  return (legPower / STAT_DIVISOR) + BASE_PLAYER_SPEED;
-}
-
-float FirstPersonPlayer::getRisingSpeed() const {
-  return (legPower / STAT_DIVISOR) + BASE_PLAYER_JUMP_SPEED;
-}
-
-float FirstPersonPlayer::getJumpHeight() const {
-  return (legPower / STAT_DIVISOR) + BASE_PLAYER_JUMP_HEIGHT;
-}
-
->>>>>>> Stashed changes
