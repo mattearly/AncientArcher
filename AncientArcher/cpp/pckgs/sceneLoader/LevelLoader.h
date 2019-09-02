21277d507046395f09c514faf5f0b81c0bd071b3
Matthew Early <matthewjearly@gmail.com>
2019-09-01 22:14:05 -0400

    removed unused function declarations

--- AncientArcher/cpp/pckgs/sceneLoader/LevelLoader.h
+++ AncientArcher/cpp/pckgs/sceneLoader/LevelLoader.h
@@ -11,13 +11,4 @@ private:
   void populateBoundries(Scene& scene);
   void populateMidSpheres(Scene& scene);
   void populateWalkwayPlanes(Scene& scene);
-  void populateDemoNanosuitModel(Scene& scene);
-  void populateDemoConeModel(Scene& scene);
-  void populateDemoMonkeyModel(Scene& scene);
-  void populateDemoSphereModel(Scene& scene);
-  void populateDemoCylinderModel(Scene& scene);
-  void populateDemoBrickCylinderModel(Scene& scene);
-  void populateRobot_1_Model(Scene& scene);
-  void populateRockBall_1_Model(Scene& scene);
-  void populateRustRobotDemo_1_Model(Scene& scene);
 };
