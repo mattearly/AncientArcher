72a0f722d39d61a0c235f58c7f5ca727f5091a62
Matthew Early <matthewjearly@gmail.com>
2019-11-13 11:13:52 -0500

    add tests for changing window clear color

--- AncientArcher/cpp/tests/main.cpp
+++ AncientArcher/cpp/tests/main.cpp
@@ -1,6 +1,6 @@
 #include "../engine/AAEngine.h"
 #include "loadModel.h"
-
+#include <glm/glm.hpp>
 int main()
 {
 
@@ -9,13 +9,17 @@ int main()
   showModelHeaderInfo("C:\\Users\\matt\\Dropbox_me298414\\Dropbox\\My3DModels\\6ColorSquare.obj");
 
   // Test 2 - Main Engine
-  AAEngine engine;                                         // init engine
-  AADisplay::getInstance()->setWindowTitle("newtitle");     // test changing window title
-  AADisplay::getInstance()->setWindowSize(1024, 768, 500, 250);
-  std::shared_ptr<AAKeyInput> keys;                         // test setting up keys
+  AAEngine engine;                                                    // init engine
+  AADisplay::getInstance()->setWindowTitle("newtitle");               // test changing window title
+  AADisplay::getInstance()->setWindowSize(1024, 768, 500, 250);       // test changing window size
+  AADisplay::getInstance()->setWindowClearColor(glm::vec3(0,0,0));    //test changing window clear color
+
+  std::shared_ptr<AAKeyInput> keys;                                   // test setting up keys
   keys = std::make_shared<AAKeyInput>();
   engine.setKeyStruct(keys);
-  engine.run();                                             // test run (main loop)
+
+  engine.run();                                                       // test run (main loop)
 
   return 0;
+
 }
