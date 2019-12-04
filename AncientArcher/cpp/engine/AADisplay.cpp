6ae18f64c71d9dc8895f4c2170333c611f9c8c33
Matthew Early <matthewjearly@gmail.com>
2019-12-04 17:54:23 -0500

    updates

--- AncientArcher/cpp/engine/AADisplay.cpp
+++ AncientArcher/cpp/engine/AADisplay.cpp
@@ -1,7 +1,11 @@
 #include "AADisplay.h"
-#include <iostream>
 #include <glm/glm.hpp>
 #include "AAViewport.h"
+#include <iostream>
+
+// --------------
+// Public Methods
+// --------------
 
 AADisplay* AADisplay::getInstance()
 {
@@ -9,38 +13,25 @@ AADisplay* AADisplay::getInstance()
   return display;
 }
 
-void AADisplay::keepWindowOpen()
-{
-  glfwSetWindowShouldClose(mWindow, false);
-}
-
-void AADisplay::closeWindow()
-{
-  glfwSetWindowShouldClose(mWindow, true);
-}
-
 AADisplay::AADisplay()
 {
   initGLFW();
 
-  int mLaunchedMainScreenWindowWidth, mLaunchedMainScreenWindowHeight;
-  glfwGetMonitorWorkarea(glfwGetPrimaryMonitor(), &mXPos, &mYPos, &mLaunchedMainScreenWindowWidth, &mLaunchedMainScreenWindowHeight);
-
-  mWindow = glfwCreateWindow(
-    mLaunchedMainScreenWindowWidth,
-    mLaunchedMainScreenWindowHeight,
-    "AncientArcher",
-    nullptr,
-    nullptr
-  );
+  mWindow = glfwCreateWindow(mWindowWidth, mWindowHeight, "AncientArcher", nullptr, nullptr);
   if (!mWindow)
   {
-    std::cout << "Failed to init GLFW3 window\n";
     glfwTerminate();
+    exit(-1);
   }
 
-  int mWindowFrameSizeLeft, mWindowFrameSizeTop, mWindowFrameSizeRight, mWindowFrameSizeBottom;
-  glfwGetWindowFrameSize(mWindow, &mWindowFrameSizeLeft, &mWindowFrameSizeTop, &mWindowFrameSizeRight, &mWindowFrameSizeBottom);
+  if (!mWindowIsFullScreen)
+  {
+    setFullscreenToOff();
+  }
+  else
+  {
+    setFullscreenToOn();
+  }
 
   glfwMakeContextCurrent(mWindow);
 
@@ -50,38 +41,17 @@ AADisplay::AADisplay()
 
   initMouseScrollHandler();
 
-
   if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))  // init glad (for opengl context)
   {
-    std::cout << "failed to init glad\n";
+    //std::cout << "failed to init glad\n";
     exit(-1);
   }
-
-  setFullscreenToOff();
 
 }
 
 AADisplay::~AADisplay()
 {
   glfwTerminate();
-}
-
-void AADisplay::reshapeWindowHandler(GLFWwindow* window, int width, int height)
-{
-  mWindowWidth = width;
-  mWindowHeight = height;
-  glViewport(mXPos, mYPos, mWindowWidth, mWindowHeight);
-  std::cout << "reshape Window Handler called.\n";
-}
-
-void AADisplay::mouseHandler(GLFWwindow* window, float xpos, float ypos)
-{
-  AAControls::getInstance()->mouseMovement(xpos, ypos);
-}
-
-void AADisplay::scrollHandler(GLFWwindow* window, float xpos, float ypos)
-{
-  AAControls::getInstance()->mouseScrollWheelMovement(xpos, ypos);
 }
 
 void AADisplay::setCursorToVisible()
@@ -106,17 +76,13 @@ void AADisplay::setWindowTitle(const char* name)
 
 void AADisplay::setWindowSize(int width, int height, int xpos, int ypos)
 {
-  mWindowWidth = width;
-  mWindowHeight = height;
-  mXPos = xpos; // (xpos == 0) ? mWindowFrameSizeLeft - mWindowFrameSizeRight : xpos;
-  mYPos = ypos; // (ypos == 0) ? mWindowFrameSizeTop - mWindowFrameSizeBottom : ypos;
   glfwSetWindowMonitor(
     mWindow,
     nullptr,
-    mXPos,
-    mYPos,
-    mWindowWidth,
-    mWindowHeight,
+    xpos,
+    ypos,
+    width,
+    height,
     0
   );
   mWindowIsFullScreen = false;
@@ -126,56 +92,9 @@ void AADisplay::setWindowClearColor(glm::vec3 rgb)
 {
   if (rgb.x < 0.f || rgb.x > 1.0f || rgb.y < 0.f || rgb.y > 1.0f || rgb.z < 0.f || rgb.z > 1.0f)
   {
-    std::cout << "WARNING: Out of range value on setWindowClearColor, values should be between 0.f and 1.f\n";
+    //std::cout << "WARNING: Out of range value on setWindowClearColor, values should be between 0.f and 1.f\n";
   }
   mWindowClearColor = rgb;
-}
-
-void AADisplay::setFullscreenToOn()
-{
-  int x, y, w, h;
-  glfwGetMonitorWorkarea(glfwGetPrimaryMonitor(), &x, &y, &w, &h);
-
-  mXPos = x;
-  mYPos = y;
-  mWindowWidth = w;
-  mWindowHeight = h;
-
-  glfwSetWindowMonitor(mWindow, glfwGetPrimaryMonitor(), 0, 0, mWindowWidth, mWindowHeight, 0);
-  
-  glViewport(mXPos, mYPos, mWindowWidth, mWindowHeight);
-  
-  mWindowIsFullScreen = true;
-}
-
-void AADisplay::setFullscreenToOff()
-{
-  //mXPos = 0; // mWindowFrameSizeLeft - mWindowFrameSizeRight;
-  //mYPos = 0; // mWindowFrameSizeTop - mWindowFrameSizeBottom;
-  //mWindowWidth = mLaunchedMainScreenWindowWidth;
-  //mWindowHeight = mLaunchedMainScreenWindowHeight;
-  glfwSetWindowMonitor(
-    mWindow,
-    nullptr,
-    mXPos,
-    mYPos,
-    mWindowWidth,
-    mWindowHeight,
-    0
-  );
-  glViewport(mXPos, mYPos, mWindowWidth, mWindowHeight);
-  mWindowIsFullScreen = false;
-}
-
-void AADisplay::clearBackBuffer() const
-{
-  glClearColor(mWindowClearColor.x, mWindowClearColor.y, mWindowClearColor.z, 0.0f);
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-}
-
-void AADisplay::swapWindowBuffers() const
-{
-  glfwSwapBuffers(mWindow);
 }
 
 int AADisplay::getScreenWidth()
@@ -198,6 +117,10 @@ GLFWwindow* AADisplay::getWindow()
   return mWindow;
 }
 
+// -----------------
+// Private Functions
+// -----------------
+
 void AADisplay::toggleFullScreen()
 {
   if (mWindowIsFullScreen)
@@ -208,6 +131,62 @@ void AADisplay::toggleFullScreen()
   {
     setFullscreenToOn();
   }
+}
+
+void AADisplay::setFullscreenToOn()
+{
+  int x, y, w, h;
+  glfwGetMonitorWorkarea(glfwGetPrimaryMonitor(), &x, &y, &w, &h);
+  int frameSizeLeft, frameSizeTop, frameSizeRight, frameSizeBot;
+  glfwGetWindowFrameSize(mWindow, &frameSizeLeft, &frameSizeTop, &frameSizeRight, &frameSizeBot);
+
+  mXPos = 0;
+  mYPos = 0;
+  mWindowWidth = w + x + frameSizeLeft + frameSizeRight;
+  mWindowHeight = h + y + frameSizeTop + frameSizeBot;
+
+  glfwSetWindowMonitor(mWindow, glfwGetPrimaryMonitor(), mXPos, mYPos, mWindowWidth, mWindowHeight, 0);
+
+  mWindowIsFullScreen = true;
+}
+
+void AADisplay::setFullscreenToOff()
+{
+  int x, y, w, h;
+  glfwGetMonitorWorkarea(glfwGetPrimaryMonitor(), &x, &y, &w, &h);
+  int frameSizeLeft, frameSizeTop, frameSizeRight, frameSizeBot;
+  glfwGetWindowFrameSize(mWindow, &frameSizeLeft, &frameSizeTop, &frameSizeRight, &frameSizeBot);
+
+  //int xPos = x;
+  //int yPos = y;
+  //int winWidth = w - frameSizeLeft - frameSizeRight;
+  //int winHeight = h - frameSizeTop - frameSizeBot;
+
+  glfwSetWindowMonitor(mWindow, nullptr, x, y - frameSizeTop, w, h - frameSizeTop, 0);
+  //glfwMaximizeWindow(mWindow);
+
+  mWindowIsFullScreen = false;
+}
+
+void AADisplay::clearBackBuffer() const
+{
+  glClearColor(mWindowClearColor.x, mWindowClearColor.y, mWindowClearColor.z, 0.0f);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+void AADisplay::swapWindowBuffers() const
+{
+  glfwSwapBuffers(mWindow);
+}
+
+void AADisplay::keepWindowOpen()
+{
+  glfwSetWindowShouldClose(mWindow, false);
+}
+
+void AADisplay::closeWindow()
+{
+  glfwSetWindowShouldClose(mWindow, true);
 }
 
 void AADisplay::initGLFW()
@@ -221,6 +200,32 @@ void AADisplay::initGLFW()
 #endif
 }
 
+// ---------------
+// GLFW3 callbacks
+// ---------------
+
+void AADisplay::reshapeWindowHandler(GLFWwindow* window, int width, int height)
+{
+  //std::cout << "reshape window called: " << width << ", " << height << '\n';
+
+  mWindowWidth = width;
+  mWindowHeight = height;
+  glViewport(0, 0, mWindowWidth, mWindowHeight);
+  AAViewport::getInstance()->setToPerspective();  // hack
+
+  //std::cout << "reshape window finished: " << mWindowWidth << ", " << mWindowHeight << '\n';
+
+}
+
+void AADisplay::mouseHandler(GLFWwindow* window, float xpos, float ypos)
+{
+  AAControls::getInstance()->mouseMovement(xpos, ypos);
+}
+
+void AADisplay::scrollHandler(GLFWwindow* window, float xpos, float ypos)
+{
+  AAControls::getInstance()->mouseScrollWheelMovement(xpos, ypos);
+}
 
 extern "C" void reshapeCallback(GLFWwindow * window, int w, int h)
 {
@@ -237,7 +242,6 @@ extern "C" void scrollCallback(GLFWwindow * window, double xpos, double ypos)
   AADisplay::getInstance()->scrollHandler(window, (float)xpos, (float)ypos);
 }
 
-
 void AADisplay::initReshapeWindowHandler()
 {
   ::glfwSetFramebufferSizeCallback(mWindow, ::reshapeCallback);
@@ -252,4 +256,3 @@ void AADisplay::initMouseScrollHandler()
 {
   ::glfwSetScrollCallback(mWindow, ::scrollCallback);
 }
-
--- AncientArcher/cpp/engine/AADisplay.h
+++ AncientArcher/cpp/engine/AADisplay.h
@@ -25,13 +25,10 @@ public:
   void setWindowTitle(const char* name);
   void setWindowSize(int width, int height, int xpos = 0, int ypos = 0);
   void setWindowClearColor(glm::vec3 rgb);
-  void setFullscreenToOn();
-  void setFullscreenToOff();
 
   int getScreenWidth();
   int getScreenHeight();
   bool getIsWindowFullScreen();
-
   GLFWwindow* getWindow();
 
   friend class AAEngine;
@@ -39,6 +36,8 @@ public:
 private:
 
   void toggleFullScreen();
+  void setFullscreenToOn();
+  void setFullscreenToOff();
 
   void clearBackBuffer() const;
   void swapWindowBuffers() const;
@@ -47,16 +46,16 @@ private:
   void closeWindow();
 
   bool mWindowIsFullScreen = false;
-  int mWindowWidth = 0, mWindowHeight = 0;
+  int mWindowWidth = 800, mWindowHeight = 600;
   int mXPos = 0, mYPos = 0;
-  GLFWmonitor* mMonitor = nullptr;
+  //GLFWmonitor* mMonitor = nullptr;
   GLFWwindow* mWindow = nullptr;
+  glm::vec3 mWindowClearColor = glm::vec3(0.35f, 0.15f, 0.35f);
 
   //int mLaunchedMainScreenWindowWidth = 0;
   //int mLaunchedMainScreenWindowHeight = 0;
   //int mWindowFrameSizeLeft = 0, mWindowFrameSizeTop = 0, mWindowFrameSizeRight = 0, mWindowFrameSizeBottom = 0;
-
-  glm::vec3 mWindowClearColor = glm::vec3(0.35f, 0.15f, 0.35f);
+  
   void initGLFW();
   void initReshapeWindowHandler();
   void initMouseHandler();
