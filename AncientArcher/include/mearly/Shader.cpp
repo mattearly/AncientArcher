6f76fcdffd5b01fdb376ead53b99dfcac85143a0
Matthew Early <matthewjearly@gmail.com>
2019-12-14 19:11:08 -0500

    setVec3 takes const vec3

--- AncientArcher/include/mearly/Shader.cpp
+++ AncientArcher/include/mearly/Shader.cpp
@@ -268,7 +268,7 @@ void Shader::setVec2(const std::string& name, float x, float y) const
   glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y);
 }
 
-void Shader::setVec3(const std::string& name, glm::vec3& value) const
+void Shader::setVec3(const std::string& name, const glm::vec3& value) const
 {
   glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
 }
--- AncientArcher/include/mearly/Shader.h
+++ AncientArcher/include/mearly/Shader.h
@@ -18,7 +18,7 @@ public:
   void setFloat(const std::string& name, float value) const;
   void setVec2(const std::string& name, glm::vec2& value) const;
   void setVec2(const std::string& name, float x, float y) const;
-  void setVec3(const std::string& name, glm::vec3& value) const;
+  void setVec3(const std::string& name, const glm::vec3& value) const;
   void setVec3(const std::string& name, float x, float y, float z) const;
   void setVec4(const std::string& name, glm::vec4& value) const;
   void setVec4(const std::string& name, float x, float y, float z, float w) const;
