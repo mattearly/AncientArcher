32918ddc2fb6909476c3e2a5a2fbfdb346df1491
Matthew Early <matthewjearly@gmail.com>
2019-12-23 21:02:38 -0500

    use direct access

--- AncientArcher/cpp/tests/dry/addPreferedDefaults.h
+++ AncientArcher/cpp/tests/dry/addPreferedDefaults.h
@@ -1,13 +1,9 @@
 #pragma once
 #include "../../engine/AAEngine.h"
 
-
 void addPreferredDefaults(AAEngine& engine)
 {
-  auto preferredDefaults = []() {
-    AAViewport::getInstance()->setRenderDistance(3000.f);
-    AADisplay::getInstance()->setCursorToDisabled();
-    AADisplay::getInstance()->setWindowSize(800, 600, 1000, 200);
-  };
-  engine.addToOnBegin(preferredDefaults);
+  AAViewport::getInstance()->setRenderDistance(3000.f);
+  AADisplay::getInstance()->setCursorToDisabled();
+  AADisplay::getInstance()->setWindowSize(800, 600, 1000, 200);
 }
