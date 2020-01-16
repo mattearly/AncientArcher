e38d0aec949b9e6a5b90ae15157ca4a9434f4ffb
Matthew Early <matthewjearly@gmail.com>
2020-01-16 03:17:58 -0500

    update shaders

--- AncientArcher/shader/frag_colored.glsl
+++ AncientArcher/shader/frag_colored.glsl
@@ -1,13 +1,10 @@
 #version 330 core
 in vec3 fragPos;
-in vec3 norm;
+in vec3 color;
 
 out vec4 fragColor;
 
-uniform vec3 color;
-
 void main()
 {
-//  vec3 tmpcolor = vec3(.5, .1, .1);
   fragColor = vec4(color, 1.0);
 }
--- AncientArcher/shader/vert_colored.glsl
+++ AncientArcher/shader/vert_colored.glsl
@@ -1,11 +1,9 @@
 #version 330 core
 layout(location = 0) in vec3 inPos;
-layout(location = 1) in vec2 inTexUV;
-layout(location = 2) in vec3 inNorm;
+layout(location = 1) in vec4 inColor;
 
 out vec3 fragPos;
-out vec2 texUV;
-out vec3 norm;
+out vec4 fragColor;
 
 uniform mat4 projection;
 uniform mat4 model;
@@ -14,8 +12,7 @@ uniform mat4 view;
 void main()
 {
   fragPos = vec3(model * vec4(inPos, 1.0));
-  texUV = inTexUV;
-  norm = inNorm;
+  fragColor = inColor;
 
   gl_Position = projection * view * model * vec4(inPos, 1.0);
 }
