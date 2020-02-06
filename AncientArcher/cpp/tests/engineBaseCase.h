7971987e5ee37e60f63a774771dfc514a54bf9e6
Matthew Early <matthewjearly@gmail.com>
2020-02-06 14:58:40 -0500

    change project structure

--- AncientArcher/cpp/tests/engineBaseCase.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "../engine/AAEngine.h"
-
-void testEngineBaseCase()
-{
-  AAEngine engineInstance;
-  DISPLAY->setWindowTitle("AA Base Test - Close Window To Continue");
-  engineInstance.run();
-}
