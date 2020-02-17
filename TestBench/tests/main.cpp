b0e2b9c9de2c21193f9b2f44940e64455b063d64
Matthew Early <matthewjearly@gmail.com>
2020-02-17 09:38:32 -0500

    test demo world that should run from a checkout

--- TestBench/tests/main.cpp
+++ TestBench/tests/main.cpp
@@ -1,17 +1,13 @@
 #include "dry/delay.h"
-#include <testlib.h>
 #include "assimpModel.h"
 #include "engineLoopBase.h"
 #include "engineDemoWorld.h"
 
 int runAllTests()
 {
-  testAssimpBaseLoading("../assets/6ColorSquare.obj");
-
-  std::cout << testlib::ret1() << '\n';
-
-  delay(120);
-  testEngineLoopBase();
+  //testAssimpBaseLoading("../assets/6ColorSquare.obj");
+  //delay(120);
+  //testEngineLoopBase();
   delay(120);
   testEngineDemoWorld();
 
