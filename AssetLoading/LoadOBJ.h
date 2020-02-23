678251cf007882754fc7a1ce233664cd0cdb5a33
Matthew Early <matthewjearly@gmail.com>
2020-02-23 15:44:38 -0500

    sample obj loader

--- AssetLoading/LoadOBJ.h
+++ AssetLoading/LoadOBJ.h
@@ -1,13 +1,13 @@
-#pragma once
-#include <glm/glm.hpp>
-#include <vector>
-
-class LoadOBJ
-{
-public:
-  static bool loadOBJ(const char* path,
-    std::vector<glm::vec3>& out_verts,
-    std::vector<glm::vec2>& out_uvs,
-    std::vector<glm::vec3>& out_norms);
-};
-
+//#pragma once
+//#include <glm/glm.hpp>
+//#include <vector>
+//
+//class LoadOBJ
+//{
+//public:
+//  static bool loadOBJ(const char* path,
+//    std::vector<glm::vec3>& out_verts,
+//    std::vector<glm::vec2>& out_uvs,
+//    std::vector<glm::vec3>& out_norms);
+//};
+//
