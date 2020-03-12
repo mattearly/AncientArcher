a77dafe47e26ffb1ad5f8c3b5bd08e4de497f731
Matthew Early <matthewjearly@gmail.com>
2020-03-12 06:48:26 -0400

    update dots

--- AABench/tests/coreTests.h
+++ AABench/tests/coreTests.h
@@ -16,15 +16,17 @@ extern AAWorld mainWorld;
 int testEngineLoopBase()
 {
   mainWorld.setWindowTitle("demo loop base");
+
   // Add to loop functions.
   mainWorld.addToOnBegin([]()noexcept {printf("World Started. timestamp: %s \n", __TIMESTAMP__); });
   mainWorld.addToDeltaUpdate([](float step)noexcept {static float timePassed = 0; timePassed += step; const float newcolor = abs(sin(timePassed)); AADisplay::getInstance()->setWindowClearColor(glm::vec3(newcolor, newcolor, newcolor)); });
   mainWorld.addToDeltaUpdate([](float step) {std::cout << ".      process delta update: " << step << '\n'; });
-  mainWorld.addToTimedOutKeyHandling([](AAKeyBoardInput& keys)noexcept {printf("...    %fms timedout keyboard/mouse\n", 90.f); return false;  });
-  mainWorld.addToKeyHandling([](AAKeyBoardInput& keys) {std::cout << "....   process keyboard/mouse \n";  });
-  mainWorld.addToScrollHandling([](AAScrollInput& scroll) {std::cout << ".....  process mouse scroll: X:" << scroll.xOffset << " Y:" << scroll.yOffset << '\n'; });
-  mainWorld.addToMouseHandling([](AAMouseInput& pointer) {std::cout << "...... process mouse pointer: X:" << pointer.xOffset << " Y:" << pointer.yOffset << '\n';  });
-  mainWorld.addToUpdate([]() {std::cout << ".......process update\n"; });
+  mainWorld.addToTimedOutKeyHandling([](AAKeyBoardInput& keys)noexcept {printf("..     %fms timedout keyboard/mouse\n", 90.f); return false;  });
+  mainWorld.addToKeyHandling([](AAKeyBoardInput& keys) {std::cout << "...    process keyboard/mouse \n";  });
+  mainWorld.addToScrollHandling([](AAScrollInput& scroll) {std::cout << "....   process mouse scroll: X:" << scroll.xOffset << " Y:" << scroll.yOffset << '\n'; });
+  mainWorld.addToMouseHandling([](AAMouseInput& pointer) {std::cout << ".....  process mouse pointer: X:" << pointer.xOffset << " Y:" << pointer.yOffset << '\n';  });
+  mainWorld.addToUpdate([]() {std::cout << "...... process update\n"; });
+
   return mainWorld.runMainLoop();
 }
 
