07b3466916695e272f0a36c1b7cf87630fd7b353
Matthew Early <matthewjearly@gmail.com>
2020-03-12 22:05:47 -0400

    allow jpegs too

--- AABench/core/objectSys/stb_image.cpp
+++ AABench/core/objectSys/stb_image.cpp
@@ -5,15 +5,15 @@
 
 //#define STBI_NO_JPEG
 //#define STBI_NO_PNG
-//#define STBI_NO_BMP
-//#define STBI_NO_PSD
-//#define STBI_NO_TGA
-//#define STBI_NO_GIF
-//#define STBI_NO_HDR
-//#define STBI_NO_PIC
-//#define STBI_NO_PNM   (.ppm and .pgm)
+#define STBI_NO_BMP
+#define STBI_NO_PSD
+#define STBI_NO_TGA
+#define STBI_NO_GIF
+#define STBI_NO_HDR
+#define STBI_NO_PIC
+#define STBI_NO_PNM   (.ppm and .pgm)
 
-#define STBI_ONLY_PNG
+//#define STBI_ONLY_PNG
 
 #define STBI_WINDOWS_UTF8
 
