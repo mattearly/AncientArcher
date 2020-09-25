0050cb54a81283771acb2032bd67c9e0c647ffc6
Matthew Early <matthewjearly@gmail.com>
2020-09-25 15:16:21 -0400

    reduce texture loading bug of nrComponents

--- AAEngine/loader/TexLoader.cpp
+++ AAEngine/loader/TexLoader.cpp
@@ -74,38 +74,49 @@ unsigned int TexLoader::textureFromFile(const char* filepath, bool gamma)
 
 	stbi_set_flip_vertically_on_load(0);
 
-	unsigned char* data = stbi_load(filepath, &width, &height, &nrComponents, 0);
+	//unsigned char* data = stbi_load(filepath, &width, &height, &nrComponents, 0);
+	unsigned char* data = stbi_load(filepath, &width, &height, &nrComponents, STBI_rgb);
 	if (data)
 	{
-		GLenum format{};
-		if (nrComponents == 1)
-		{
-			format = GL_RED;
-		}
-		else if (nrComponents == 3)
-		{
-			format = GL_RGB;
-		}
-		else if (nrComponents == 4)
-		{
-			format = GL_RGBA;
-		}
+		//GLenum format{};
+		//if (nrComponents == 1)
+		//{
+		//	format = GL_RED;
+		//}
+		//else if (nrComponents == 3)
+		//{
+		//	format = GL_RGB;
+		//}
+		//else if (nrComponents == 4)
+		//{
+		//	format = GL_RGBA;
+		//}
 
 		glGenTextures(1, &out_texID);
 		glBindTexture(GL_TEXTURE_2D, out_texID);
 
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
+		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
-		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+		//try: https://stackoverflow.com/questions/23150123/loading-png-with-stb-image-for-opengl-texture-gives-wrong-colors
+		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+		
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
 		glGenerateMipmap(GL_TEXTURE_2D);
 
 		glBindTexture(GL_TEXTURE_2D, 0);
 
 		stbi_image_free(data);
+		std::cout << "::TEXLOADER: SUCCESS at path: " << filepath << std::endl;
+
 	}
 	else
 	{
