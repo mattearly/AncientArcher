0220aa340c9f6f9823ea222a10f38588c1808e9d
Matthew Early <matthewjearly@gmail.com>
2020-09-26 06:09:41 -0400

    allow bmp

--- AAEngine/stb_image.cpp
+++ AAEngine/stb_image.cpp
@@ -1,10 +1,10 @@
 #define STB_IMAGE_IMPLEMENTATION
 
-// png's and jpegs only
+// png, jpegs, and bmp only
 
 //#define STBI_NO_JPEG
 //#define STBI_NO_PNG
-#define STBI_NO_BMP
+//#define STBI_NO_BMP
 #define STBI_NO_PSD
 #define STBI_NO_TGA
 #define STBI_NO_GIF
