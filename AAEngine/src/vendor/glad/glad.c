f6b8c5b987bfa82b2aef464abd00f4bdf99bb3d6
Matthew Early <matthewjearly@gmail.com>
2020-10-06 20:40:44 -0400

    fix include dir

--- AAEngine/src/vendor/glad/glad.c
+++ AAEngine/src/vendor/glad/glad.c
@@ -22,7 +22,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../../include/vendor/glad/glad.h"
+#include "../../../include/vendor/glad/glad.h"
 
 static void* get_proc(const char* namez);
 
