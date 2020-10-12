07f818c0b4e3811b2d7f7fd8277ec342dcb22fee
Matthew Early <matthewjearly@gmail.com>
2020-10-12 06:39:44 -0400

    all instances have collider spheres on them

--- ClassicAstroids/include/SplitAstroid.h
+++ ClassicAstroids/include/SplitAstroid.h
@@ -11,11 +11,13 @@ void splitAstroid(int game_object)
 	AA::GameObject& our_ref = LOOP->getGameObject(game_object);
 	glm::vec3 original_loc = our_ref.getLocation();
 
-	if (our_ref.isSingleInstance()) {
+	if (our_ref.isSingleInstance())
+	{
 		our_ref.scaleTo(glm::vec3(SPLIT_SIZE));
 		for (int i = 0; i < 3; i++)
 		{
 			AA::InstanceDetails new_details(glm::vec3(SPLIT_SIZE), glm::vec3(0), original_loc + glm::vec3(NTKR(-RAND_DISTANCE, RAND_DISTANCE), 0, NTKR(-RAND_DISTANCE, RAND_DISTANCE)));
+			new_details.mColliderSphere = new ColliderSphere(new_details.Translate, our_ref.getColliderSphere()->radius);
 			our_ref.addInstance(new_details);
 		}
 	}
