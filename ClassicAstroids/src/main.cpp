78d318bc2e43e57b5540821d50b8a017b535b0ff
Matthew Early <matthewjearly@gmail.com>
2020-10-27 20:45:57 -0400

    update bullet logic, now has a cooldown and less buggy

--- ClassicAstroids/src/main.cpp
+++ ClassicAstroids/src/main.cpp
@@ -1,5 +1,4 @@
 #include <Loop.h>
-//#include <glm/glm.hpp>
 #include <Lights.h>
 #include <rand/rand.h>
 #include <CollisionHandler.h>
@@ -13,86 +12,85 @@ int main()
 	static int cam_1 = LOOP->addCamera();
 	LOOP->getCamera(cam_1).shiftYawAndPitch(0.f, -90.f); // look down
 
-	// fullscreen
 	DISPLAY->setWindowSize(800, 800, true);
 
-	static int shader_unlit = LOOP->addShader("../assets/shaders/noLight.vert", "../assets/shaders/noLight.frag");
+	static int unlit_shader = LOOP->addShader("../assets/shaders/noLight.vert", "../assets/shaders/noLight.frag");
 
-	static int shader_lit = LOOP->addShader("../assets/shaders/combinedLight.vert", "../assets/shaders/combinedLight.frag");
+	static int lit_shader = LOOP->addShader("../assets/shaders/combinedLight.vert", "../assets/shaders/combinedLight.frag");
 	// directional light for lit shader
-	static AA::DirectionalLight dLight;
-	dLight.Direction = glm::vec3(-0.45f, -1.f, 0.f);
-	dLight.Ambient = glm::vec3(0.05f);
-	dLight.Diffuse = glm::vec3(0.3f);
-	dLight.Specular = glm::vec3(1);
-	setDirectionalLight(dLight, LOOP->getShader(shader_lit));
+	static AA::DirectionalLight directional_light;
+	directional_light.Direction = glm::vec3(-0.45f, -1.f, 0.f);
+	directional_light.Ambient = glm::vec3(0.05f);
+	directional_light.Diffuse = glm::vec3(0.3f);
+	directional_light.Specular = glm::vec3(1);
+	setDirectionalLight(directional_light, LOOP->getShader(lit_shader));
 
 	// BACKDROP
-	static int go_starplane = LOOP->addObject("../assets/models/obj/starplane.obj", cam_1, shader_lit);
-	LOOP->getGameObject(go_starplane).translateTo(glm::vec3(0, -99, 0));
-	LOOP->getGameObject(go_starplane).scaleTo(glm::vec3(10, 1, 10));
+	static int starplane_object = LOOP->addObject("../assets/models/obj/starplane.obj", cam_1, lit_shader);
+	LOOP->getGameObject(starplane_object).translateTo(glm::vec3(0, -99, 0));
+	LOOP->getGameObject(starplane_object).scaleTo(glm::vec3(10, 1, 10));
 
 	// PLAYER SHIP
-	static int go_ship = LOOP->addObject("../assets/models/obj/flyingV.obj", cam_1, shader_unlit);
-	LOOP->getGameObject(go_ship).translateTo(glm::vec3(0, -20, 0));
-	LOOP->getGameObject(go_ship).scaleTo(glm::vec3(.6667f));
-	LOOP->getGameObject(go_ship).rotateTo(glm::vec3(0, glm::radians(180.f), 0));
+	static int player_ship_object = LOOP->addObject("../assets/models/obj/flyingV.obj", cam_1, unlit_shader);
+	LOOP->getGameObject(player_ship_object).translateTo(glm::vec3(0, -20, 0));
+	LOOP->getGameObject(player_ship_object).scaleTo(glm::vec3(.6667f));
+	LOOP->getGameObject(player_ship_object).rotateTo(glm::vec3(0, glm::radians(180.f), 0));
 
 	// LAZER BULLET
-	static int go_lazer = LOOP->addObject("../assets/models/obj/lazer.obj", cam_1, shader_unlit);
-	LOOP->getGameObject(go_lazer).translateTo(LOOP->getGameObject(go_ship).getLocation());
-	LOOP->getGameObject(go_lazer).scaleTo(glm::vec3(.3333f));
-	LOOP->getGameObject(go_lazer).rotateTo(glm::vec3(0, glm::radians(180.f), 0));
-	LOOP->getGameObject(go_lazer).setColliderSphere(LOOP->getGameObject(go_ship).getLocation(), .02f);
+	static int bullet_object = LOOP->addObject("../assets/models/obj/lazer.obj", cam_1, unlit_shader);
+	LOOP->getGameObject(bullet_object).translateTo(LOOP->getGameObject(player_ship_object).getLocation());
+	LOOP->getGameObject(bullet_object).scaleTo(glm::vec3(.3333f));
+	LOOP->getGameObject(bullet_object).rotateTo(glm::vec3(0, glm::radians(180.f), 0));
+	LOOP->getGameObject(bullet_object).setColliderSphere(LOOP->getGameObject(player_ship_object).getLocation(), .02f);
 
 	static bool turnleft(false), turnright(false), moveforward(false), fireweap(false);
-	auto hotkeys = [](AA::KeyboardInput& keyin)
+	auto hotkeys = [](AA::KeyboardInput& keypress)
 	{
-		if (keyin.w || keyin.upArrow)  // move in pointing direction
+		if (keypress.w || keypress.upArrow)  // move in pointing direction
 		{
 			moveforward = true;
 		}
-		else if (!(keyin.w || keyin.upArrow))
+		else if (!(keypress.w || keypress.upArrow))
 		{
 			moveforward = false;
 		}
 
-		if (keyin.a || keyin.leftArrow)  // turn left
+		if (keypress.a || keypress.leftArrow)  // turn left
 		{
 			turnleft = true;
 		}
-		else if (!(keyin.a || keyin.leftArrow))
+		else if (!(keypress.a || keypress.leftArrow))
 		{
 			turnleft = false;
 		}
 
-		if (keyin.d || keyin.rightArrow)  // turn right
+		if (keypress.d || keypress.rightArrow)  // turn right
 		{
 			turnright = true;
 		}
-		else if (!(keyin.d || keyin.rightArrow))
+		else if (!(keypress.d || keypress.rightArrow))
 		{
 			turnright = false;
 		}
 
-		if (keyin.spacebar || keyin.mouseButton1)
+		if (keypress.spacebar || keypress.mouseButton1)
 		{
 			fireweap = true;
 		}
-		else if (!(keyin.spacebar || keyin.mouseButton1))
+		else if (!(keypress.spacebar || keypress.mouseButton1))
 		{
 			fireweap = false;
 		}
 	};
 	LOOP->addToKeyHandling(hotkeys);
 
-	static const float BOUNDRYSIZE = 10.9f;
+	static const float BOUNDRYSIZE = 11.1f;
 
 	const float TURNSPEED = 210.f;
 	static const float TURNSPEEDr = glm::radians(TURNSPEED);
 	static const float MOVESPEED = 6.9f;
-	static const float FIRELENGTH = .3187f;
-	static const float BULLETSPEED = 23.f;
+	static const float BULLETCOOLDOWN = .4187f;
+	static const float BULLETSPEED = 21.f;
 
 	static float xShipDir;
 	static float zShipDir;
@@ -100,110 +98,131 @@ int main()
 	static float zCamPos;
 	static float xFireDir;
 	static float zFireDir;
+
 	static bool bulletOut(false);
-	static float bulletTimer = 0.f;
 	static bool bulletHitSomething(false);
+
+	static bool bulletOnCooldown(false);
+	static float bulletCooldownLength = 0.f;
+
+	static auto resetBullet = []() {
+		// reset variables
+		bulletOut = false; bulletHitSomething = false;
+		// reset location
+		LOOP->getGameObject(bullet_object).translateTo(LOOP->getGameObject(player_ship_object).getLocation());
+		LOOP->getGameObject(bullet_object).rotateTo(LOOP->getGameObject(player_ship_object).getRotation());
+	};
 
 	auto controlShip = [](float dt) {
 		if (moveforward)
 		{
-			glm::vec3 dir = LOOP->getGameObject(go_ship).getRotation();
+			glm::vec3 dir = LOOP->getGameObject(player_ship_object).getRotation();
 			xShipDir = sin(dir.y);
 			zShipDir = cos(dir.y);
-			LOOP->getGameObject(go_ship).advanceTranslate(glm::vec3(xShipDir, 0, zShipDir) * dt * MOVESPEED);
-
-			// cam follow if move
-//xCamPos = LOOP->getGameObject(go_ship).getLocation().x;
-//zCamPos = LOOP->getGameObject(go_ship).getLocation().z;
-//LOOP->getCamera(cam_1).setCurrentPosition(glm::vec3(xCamPos, 0, zCamPos));
-//std::cout << "loc" << xCamPos << ',' << zCamPos << '\n';
+			LOOP->getGameObject(player_ship_object).advanceTranslate(glm::vec3(xShipDir, 0, zShipDir) * dt * MOVESPEED);
 
 			//positive bounds
-			if (LOOP->getGameObject(go_ship).getLocation().x > BOUNDRYSIZE)
-				LOOP->getGameObject(go_ship).advanceTranslate(glm::vec3(-BOUNDRYSIZE * 2, 0, 0));
+			if (LOOP->getGameObject(player_ship_object).getLocation().x > BOUNDRYSIZE)
+				LOOP->getGameObject(player_ship_object).advanceTranslate(glm::vec3(-BOUNDRYSIZE * 2, 0, 0));
 
-			if (LOOP->getGameObject(go_ship).getLocation().z > BOUNDRYSIZE)
-				LOOP->getGameObject(go_ship).advanceTranslate(glm::vec3(0, 0, -BOUNDRYSIZE * 2));
+			if (LOOP->getGameObject(player_ship_object).getLocation().z > BOUNDRYSIZE)
+				LOOP->getGameObject(player_ship_object).advanceTranslate(glm::vec3(0, 0, -BOUNDRYSIZE * 2));
 
 			//negative bounds
-			if (LOOP->getGameObject(go_ship).getLocation().x < -BOUNDRYSIZE)
-				LOOP->getGameObject(go_ship).advanceTranslate(glm::vec3(BOUNDRYSIZE * 2, 0, 0));
+			if (LOOP->getGameObject(player_ship_object).getLocation().x < -BOUNDRYSIZE)
+				LOOP->getGameObject(player_ship_object).advanceTranslate(glm::vec3(BOUNDRYSIZE * 2, 0, 0));
 
-			if (LOOP->getGameObject(go_ship).getLocation().z < -BOUNDRYSIZE)
-				LOOP->getGameObject(go_ship).advanceTranslate(glm::vec3(0, 0, BOUNDRYSIZE * 2));
+			if (LOOP->getGameObject(player_ship_object).getLocation().z < -BOUNDRYSIZE)
+				LOOP->getGameObject(player_ship_object).advanceTranslate(glm::vec3(0, 0, BOUNDRYSIZE * 2));
+
+			if (!bulletOut)
+			{
+				//apply loc to bullet if it is not out
+				LOOP->getGameObject(bullet_object).translateTo(LOOP->getGameObject(player_ship_object).getLocation());
+			}
+
 		}
 
 		if (turnleft)
 		{
-			LOOP->getGameObject(go_ship).advanceRotation(glm::vec3(0, TURNSPEEDr * dt, 0));
+			LOOP->getGameObject(player_ship_object).advanceRotation(glm::vec3(0, TURNSPEEDr * dt, 0));
+			if (!bulletOut)
+			{
+				//apply rotation to bullet if it is not out
+				LOOP->getGameObject(bullet_object).rotateTo(LOOP->getGameObject(player_ship_object).getRotation());
+			}
+
 		}
 
 		if (turnright)
 		{
-			LOOP->getGameObject(go_ship).advanceRotation(glm::vec3(0, -TURNSPEEDr * dt, 0));
+			LOOP->getGameObject(player_ship_object).advanceRotation(glm::vec3(0, -TURNSPEEDr * dt, 0));
+			if (!bulletOut)
+			{
+				//apply rotation to bullet if it is not out
+				LOOP->getGameObject(bullet_object).rotateTo(LOOP->getGameObject(player_ship_object).getRotation());
+			}
+
 		}
 
-		if (fireweap && !bulletOut)
+		// bullet logic
+		if (fireweap && !bulletOnCooldown)
 		{
 			bulletOut = true;
-			glm::vec3 dir = LOOP->getGameObject(go_ship).getRotation();
+			bulletOnCooldown = true;
+			glm::vec3 dir = LOOP->getGameObject(player_ship_object).getRotation();
 			xShipDir = sin(dir.y);
 			zShipDir = cos(dir.y);
 			xFireDir = xShipDir;
 			zFireDir = zShipDir;
 		}
 
-		if (bulletOut)
+		if (bulletOnCooldown)
 		{
-			bulletTimer += dt;
+			bulletCooldownLength += dt;
 
-			if (bulletTimer >= FIRELENGTH)
+			if (bulletCooldownLength > BULLETCOOLDOWN)
 			{
-				bulletOut = false;
-				bulletTimer = 0.f;
-				bulletHitSomething = false;
+				bulletOnCooldown = false;
+				bulletCooldownLength = 0.f;
+				if (bulletOut)  //otherwise it is already reset
+				{
+					resetBullet();
+				}
 			}
 
-			if (!bulletHitSomething)
+			if (bulletOut)
 			{
-				LOOP->getGameObject(go_lazer).advanceTranslate(glm::vec3(xFireDir, 0, zFireDir) * dt * BULLETSPEED);
+				if (!bulletHitSomething)
+				{
+					LOOP->getGameObject(bullet_object).advanceTranslate(glm::vec3(xFireDir, 0, zFireDir) * dt * BULLETSPEED);
 
-				//positive bounds
-				if (LOOP->getGameObject(go_lazer).getLocation().x > BOUNDRYSIZE)
-					LOOP->getGameObject(go_lazer).advanceTranslate(glm::vec3(-BOUNDRYSIZE * 2, 0, 0));
+					//positive bounds
+					if (LOOP->getGameObject(bullet_object).getLocation().x > BOUNDRYSIZE)
+						LOOP->getGameObject(bullet_object).advanceTranslate(glm::vec3(-BOUNDRYSIZE * 2, 0, 0));
 
-				if (LOOP->getGameObject(go_lazer).getLocation().z > BOUNDRYSIZE)
-					LOOP->getGameObject(go_lazer).advanceTranslate(glm::vec3(0, 0, -BOUNDRYSIZE * 2));
+					if (LOOP->getGameObject(bullet_object).getLocation().z > BOUNDRYSIZE)
+						LOOP->getGameObject(bullet_object).advanceTranslate(glm::vec3(0, 0, -BOUNDRYSIZE * 2));
 
-				//negative bounds
-				if (LOOP->getGameObject(go_lazer).getLocation().x < -BOUNDRYSIZE)
-					LOOP->getGameObject(go_lazer).advanceTranslate(glm::vec3(BOUNDRYSIZE * 2, 0, 0));
+					//negative bounds
+					if (LOOP->getGameObject(bullet_object).getLocation().x < -BOUNDRYSIZE)
+						LOOP->getGameObject(bullet_object).advanceTranslate(glm::vec3(BOUNDRYSIZE * 2, 0, 0));
 
-				if (LOOP->getGameObject(go_lazer).getLocation().z < -BOUNDRYSIZE)
-					LOOP->getGameObject(go_lazer).advanceTranslate(glm::vec3(0, 0, BOUNDRYSIZE * 2));
+					if (LOOP->getGameObject(bullet_object).getLocation().z < -BOUNDRYSIZE)
+						LOOP->getGameObject(bullet_object).advanceTranslate(glm::vec3(0, 0, BOUNDRYSIZE * 2));
+				}
 			}
-			else
-			{
-				//apply steady loc & rotation
-				LOOP->getGameObject(go_lazer).translateTo(LOOP->getGameObject(go_ship).getLocation());
-				LOOP->getGameObject(go_lazer).rotateTo(LOOP->getGameObject(go_ship).getRotation());
-			}
-		}
-		else
-		{
-			//apply steady loc & rotation
-			LOOP->getGameObject(go_lazer).translateTo(LOOP->getGameObject(go_ship).getLocation());
-			LOOP->getGameObject(go_lazer).rotateTo(LOOP->getGameObject(go_ship).getRotation());
+
 		}
 	};
 	LOOP->addToDeltaUpdate(controlShip);
 
 	// ASTROIDS
-	static int go_asteroid = LOOP->addObject("../assets/models/obj/asteroid.obj", cam_1, shader_unlit);
+	static int go_asteroid = LOOP->addObject("../assets/models/obj/asteroid.obj", cam_1, unlit_shader);
 	LOOP->getGameObject(go_asteroid).translateTo(glm::vec3(0, -20, -5));
 	LOOP->getGameObject(go_asteroid).setColliderSphere(glm::vec3(0, -20, -5), 1.f);
 
-	static int go_asteroid2 = LOOP->addObject("../assets/models/obj/asteroid2.obj", cam_1, shader_unlit);
+	static int go_asteroid2 = LOOP->addObject("../assets/models/obj/asteroid2.obj", cam_1, unlit_shader);
 	LOOP->getGameObject(go_asteroid2).translateTo(glm::vec3(-7, -20, -7));
 	LOOP->getGameObject(go_asteroid2).setColliderSphere(glm::vec3(-7, -20, -7), 1.f);
 
@@ -218,12 +237,11 @@ int main()
 			{
 				if (AA::CollisionHandler::getInstance()->sphere_vs_Sphere_3D
 				(
-					LOOP->getGameObject(go_lazer).getColliderSphere(),
+					LOOP->getGameObject(bullet_object).getColliderSphere(),
 					LOOP->getGameObject(ast.object_id).getColliderSphere(ast.instance_id)
 				))
 				{
-					//std::cout << "hit! obj id: " << ast.object_id << ", inst id: " << ast.instance_id << '\n';
-					bulletHitSomething = true;
+					resetBullet();
 					hitAstroid(ast, astroids);
 					return;
 				}
