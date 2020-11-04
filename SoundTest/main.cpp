36ee2de91c52daaee025e95c965331a68fd42f50
Matthew Early <matthewjearly@gmail.com>
2020-11-03 20:54:36 -0500

    correct names

--- SoundTest/main.cpp
+++ SoundTest/main.cpp
@@ -9,14 +9,12 @@ using namespace AA;
 int main()
 {
 	SoundDevice* sound_dev = SoundDevice::get(); // should init sound device and not throw if successful
-	static uint32_t ding = SoundBuffers::get()->addSound("../assets/sounds/heal.ogg");
-	static SoundSource cowbellding;
+	static uint32_t heal_sound_buf = SoundBuffers::get()->addSound("../assets/sounds/heal.ogg");
+	static SoundSource heal_sound;
 	auto playDingOnClick = [](KeyboardInput& in) {
 		if (in.mouseButton1)
 		{
-			std::cout << "play ding\n";
-
-			cowbellding.Play(ding);
+			heal_sound.Play(heal_sound_buf);
 		}
 	};
 	LOOP->addToKeyHandling(playDingOnClick);
