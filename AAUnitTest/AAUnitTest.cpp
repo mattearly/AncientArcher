3260702a4c02e35fc93bae8a7f7b56ff0f927e3e
Matthew Early <matthewjearly@gmail.com>
2021-01-19 17:22:07 -0500

    update and add tests

--- AAUnitTest/AAUnitTest.cpp
+++ AAUnitTest/AAUnitTest.cpp
@@ -15,12 +15,13 @@ public:
 	{
 		static float timeholder_A = 0.f;
 
-		AA::Engine->AddToDeltaUpdate([](float dt) {
-			timeholder_A += dt;
-			if (timeholder_A > 3.f)
+		AA::Engine->AddToDeltaUpdate([](float dt)
 			{
-				AA::Engine->Shutdown();
-			}
+				timeholder_A += dt;
+				if (timeholder_A > 3.f)
+				{
+					AA::Engine->Shutdown();
+				}
 			});
 		Assert::AreEqual(AA::Engine->Run(), 0);
 	}
@@ -47,23 +48,16 @@ public:
 		static int cam = AA::Engine->AddCamera(AA::Engine->GetWindowWidth(), AA::Engine->GetWindowHeight());
 		int shader = AA::Engine->AddShader(AA::SHADERTYPE::DIFF);
 
-		//AA::Engine->AddObject("E:\\storage\\Models\\dovecote.fbx", cam, shader);
-
 		int dovecote = AA::Engine->AddObject("E:\\storage\\Models\\dovecote.fbx", cam, shader);
 		AA::Engine->GetGameObject(dovecote).setTranslation(glm::vec3(0, 0, 0));
 
-		//AA::Engine->GetGameObject(chicken).setScale(glm::vec3(.2,.2,.2));
-
-		AA::Engine->GetCamera(cam).setCurrentPosition(glm::vec3(0, 250, 0));
+		AA::Engine->GetCamera(cam).setCurrentPosition(glm::vec3(0, 450, 300));
 		AA::Engine->GetCamera(cam).setCurrentPitch(0.f);
-		AA::Engine->GetCamera(cam).setCurrentYaw(90.f);
-
-		//AA::Engine->SetMaxRenderDistance(cam, 4000.f);
+		AA::Engine->GetCamera(cam).setCurrentYaw(270.f);
 
 		AA::Engine->AddToDeltaUpdate([](float dt) {
-			//AA::Engine->GetCamera(cam).shiftYawAndPitch(dt*100, 0);
-			AA::Engine->GetCamera(cam).shiftCurrentPosition(glm::vec3(0, 0, -dt * 200));
-			if (AA::Engine->GetCamera(cam).getLocation().z < -2000)
+			AA::Engine->GetCamera(cam).shiftCurrentPosition(glm::vec3(0, 0, dt * 200));
+			if (AA::Engine->GetCamera(cam).getLocation().z > 2300)
 			{
 				AA::Engine->Shutdown();
 			}
@@ -72,5 +66,59 @@ public:
 		Assert::AreEqual(AA::Engine->Run(), 0);
 
 	}
+
+	TEST_METHOD(D_BaseMovementTest)
+	{
+		AA::Engine->SoftReset();
+		static int cam = AA::Engine->AddCamera(AA::Engine->GetWindowWidth(), AA::Engine->GetWindowHeight());
+		int shader = AA::Engine->AddShader(AA::SHADERTYPE::DIFF);
+
+		int dovecote = AA::Engine->AddObject("E:\\storage\\Models\\dovecote.fbx", cam, shader);
+		AA::Engine->GetGameObject(dovecote).setTranslation(glm::vec3(0, 0, 0));
+
+		AA::Engine->GetCamera(cam).setCurrentPosition(glm::vec3(0, 350, 700));
+		AA::Engine->GetCamera(cam).setCurrentPitch(0.f);
+		AA::Engine->GetCamera(cam).setCurrentYaw(270.f);
+
+		static bool left, right, forward, backwards;
+
+		AA::Engine->AddToKeyHandling([](AA::KeyboardInput& kb) {
+			if (kb.w)
+				forward = true;
+			else
+				forward = false;
+
+			if (kb.s)
+				backwards = true;
+			else
+				backwards = false;
+
+			if (kb.a)
+				left = true;
+			else
+				left = false;
+
+			if (kb.d)
+				right = true;
+			else
+				right = false;
+			});
+		static float VELOCITY = 100;
+		AA::Engine->AddToDeltaUpdate([](float dt) {
+			if (forward)
+				AA::Engine->GetCamera(cam).shiftCurrentPosition(glm::vec3(0, 0, -dt * VELOCITY));
+			if (backwards)
+				AA::Engine->GetCamera(cam).shiftCurrentPosition(glm::vec3(0, 0, dt * VELOCITY));
+			if (left)
+				AA::Engine->GetCamera(cam).shiftCurrentPosition(glm::vec3(-dt * VELOCITY, 0, 0));
+			if (right)
+				AA::Engine->GetCamera(cam).shiftCurrentPosition(glm::vec3(dt * VELOCITY, 0, 0));
+
+			});
+
+		Assert::AreEqual(AA::Engine->Run(), 0);
+	}
+
+
 };
 }
