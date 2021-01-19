455c0fa877974c245029dc4b932279f4cd199c2a
Matthew Early <matthewjearly@gmail.com>
2021-01-19 18:13:23 -0500

    add wasd test

--- AAUnitTest/AAUnitTest.cpp
+++ AAUnitTest/AAUnitTest.cpp
@@ -42,12 +42,13 @@ public:
 		Assert::AreEqual(AA::Engine->Run(), 0);
 	}
 
-	TEST_METHOD(C_BaseCamAndModelTest)
+	TEST_METHOD(C_AutoCamAndModelTest)
 	{
 		AA::Engine->SoftReset();
 		static int cam = AA::Engine->AddCamera(AA::Engine->GetWindowWidth(), AA::Engine->GetWindowHeight());
 		int shader = AA::Engine->AddShader(AA::SHADERTYPE::DIFF);
 
+		// this is local, put in your own model to test it out
 		int dovecote = AA::Engine->AddObject("E:\\storage\\Models\\dovecote.fbx", cam, shader);
 		AA::Engine->GetGameObject(dovecote).setTranslation(glm::vec3(0, 0, 0));
 
@@ -67,53 +68,73 @@ public:
 
 	}
 
-	TEST_METHOD(D_BaseMovementTest)
+	TEST_METHOD(D_WASDMovementTest)
 	{
 		AA::Engine->SoftReset();
 		static int cam = AA::Engine->AddCamera(AA::Engine->GetWindowWidth(), AA::Engine->GetWindowHeight());
 		int shader = AA::Engine->AddShader(AA::SHADERTYPE::DIFF);
 
-		int dovecote = AA::Engine->AddObject("E:\\storage\\Models\\dovecote.fbx", cam, shader);
-		AA::Engine->GetGameObject(dovecote).setTranslation(glm::vec3(0, 0, 0));
+		//static int wasd = AA::Engine->AddObject("E:\\storage\\source\\repos\\AncientArcher\\AAUnitTest\\res\\wasd.obj", cam, shader);
+		static int wasd = AA::Engine->AddObject("..\\..\\AAUnitTest\\res\\wasd.obj", cam, shader);
+		//static int wasd = AA::Engine->AddObject("E:\\storage\\Models\\dovecote.fbx", cam, shader);
 
-		AA::Engine->GetCamera(cam).setCurrentPosition(glm::vec3(0, 350, 700));
+		AA::Engine->GetGameObject(wasd).setTranslation(glm::vec3(0, 0, 0));
+		AA::Engine->GetGameObject(wasd).setScale(glm::vec3(3));
+
+		AA::Engine->GetCamera(cam).setCurrentPosition(glm::vec3(0, 0, 30));
 		AA::Engine->GetCamera(cam).setCurrentPitch(0.f);
 		AA::Engine->GetCamera(cam).setCurrentYaw(270.f);
 
 		static bool left, right, forward, backwards;
+		static bool pressedleft, pressedright, pressedforward, pressedbackwards;
 
 		AA::Engine->AddToKeyHandling([](AA::KeyboardInput& kb) {
 			if (kb.w)
-				forward = true;
+				forward = pressedforward = true;
 			else
 				forward = false;
 
 			if (kb.s)
-				backwards = true;
+				backwards = pressedbackwards = true;
 			else
 				backwards = false;
 
 			if (kb.a)
-				left = true;
+				left = pressedleft = true;
 			else
 				left = false;
 
 			if (kb.d)
-				right = true;
+				right = pressedright = true;
 			else
 				right = false;
 			});
+
 		static float VELOCITY = 100;
+
 		AA::Engine->AddToDeltaUpdate([](float dt) {
+
 			if (forward)
 				AA::Engine->GetCamera(cam).shiftCurrentPosition(glm::vec3(0, 0, -dt * VELOCITY));
+
 			if (backwards)
 				AA::Engine->GetCamera(cam).shiftCurrentPosition(glm::vec3(0, 0, dt * VELOCITY));
+
 			if (left)
 				AA::Engine->GetCamera(cam).shiftCurrentPosition(glm::vec3(-dt * VELOCITY, 0, 0));
+
 			if (right)
 				AA::Engine->GetCamera(cam).shiftCurrentPosition(glm::vec3(dt * VELOCITY, 0, 0));
 
+			// rotate object
+			AA::Engine->GetGameObject(wasd).advanceRotation(glm::vec3(dt * .2, dt * .2, dt * .2));
+			});
+
+		AA::Engine->AddToSlowUpdate([]() {
+			if (pressedleft && pressedright && pressedforward && pressedbackwards)
+			{
+				AA::Engine->Shutdown();
+			}
 			});
 
 		Assert::AreEqual(AA::Engine->Run(), 0);
