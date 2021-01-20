99d8270f7b3079f714e1adf4bd63a211cbb03256
Matthew Early <matthewjearly@gmail.com>
2021-01-20 02:50:24 -0500

    cam updates, improve tests C & D

--- AAUnitTest/AAUnitTest.cpp
+++ AAUnitTest/AAUnitTest.cpp
@@ -52,13 +52,14 @@ public:
 		int dovecote = AA::Engine->AddObject("E:\\storage\\Models\\dovecote.fbx", cam, shader);
 		AA::Engine->GetGameObject(dovecote).setTranslation(glm::vec3(0, 0, 0));
 
-		AA::Engine->GetCamera(cam).setCurrentPosition(glm::vec3(0, 450, 300));
-		AA::Engine->GetCamera(cam).setCurrentPitch(0.f);
-		AA::Engine->GetCamera(cam).setCurrentYaw(270.f);
+		AA::Cam(cam).SetCurrentLocation(glm::vec3(0, 350, 300));
+		//AA::Cam(cam).SetCurrentPitch(0.f);
+		//AA::Cam(cam).SetCurrentYaw(0.f);
+		AA::Cam(cam).SetMaxRenderDistance(2700.f);
 
 		AA::Engine->AddToDeltaUpdate([](float dt) {
-			AA::Engine->GetCamera(cam).shiftCurrentPosition(glm::vec3(0, 0, dt * 200));
-			if (AA::Engine->GetCamera(cam).getLocation().z > 2300)
+			AA::Engine->GetCamera(cam).ShiftCurrentLocation(glm::vec3(0, 0, dt * 200));
+			if (AA::Engine->GetCamera(cam).GetLocation().z > 2300)
 			{
 				AA::Engine->Shutdown();
 			}
@@ -78,10 +79,12 @@ public:
 
 		AA::Engine->GetGameObject(wasd).setTranslation(glm::vec3(0, 0, 0));
 		AA::Engine->GetGameObject(wasd).setScale(glm::vec3(3));
+		//AA::Engine->GetGameObject(wasd).setRotation(glm::vec3(3));
+		AA::Engine->GetGameObject(wasd).advanceRotation(glm::vec3(90,0,0));
 
-		AA::Engine->GetCamera(cam).setCurrentPosition(glm::vec3(0, 0, 30));
-		AA::Engine->GetCamera(cam).setCurrentPitch(0.f);
-		AA::Engine->GetCamera(cam).setCurrentYaw(270.f);
+		AA::Engine->GetCamera(cam).SetCurrentLocation(glm::vec3(0, 0, 30));
+		AA::Engine->GetCamera(cam).SetCurrentPitch(0.f);
+		AA::Engine->GetCamera(cam).SetCurrentYaw(270.f);
 
 		static bool left, right, forward, backwards;
 		static bool pressedleft, pressedright, pressedforward, pressedbackwards;
@@ -113,19 +116,20 @@ public:
 		AA::Engine->AddToDeltaUpdate([](float dt) {
 
 			if (forward)
-				AA::Engine->GetCamera(cam).shiftCurrentPosition(glm::vec3(0, 0, -dt * VELOCITY));
+				AA::Engine->GetCamera(cam).ShiftCurrentLocation(glm::vec3(0, 0, -dt * VELOCITY));
 
 			if (backwards)
-				AA::Engine->GetCamera(cam).shiftCurrentPosition(glm::vec3(0, 0, dt * VELOCITY));
+				AA::Engine->GetCamera(cam).ShiftCurrentLocation(glm::vec3(0, 0, dt * VELOCITY));
 
 			if (left)
-				AA::Engine->GetCamera(cam).shiftCurrentPosition(glm::vec3(-dt * VELOCITY, 0, 0));
+				AA::Engine->GetCamera(cam).ShiftCurrentLocation(glm::vec3(-dt * VELOCITY, 0, 0));
 
 			if (right)
-				AA::Engine->GetCamera(cam).shiftCurrentPosition(glm::vec3(dt * VELOCITY, 0, 0));
+				AA::Engine->GetCamera(cam).ShiftCurrentLocation(glm::vec3(dt * VELOCITY, 0, 0));
 
 			// rotate object
-			AA::Engine->GetGameObject(wasd).advanceRotation(glm::vec3(dt * .2, dt * .2, dt * .2));
+			//AA::Engine->GetGameObject(wasd).advanceRotation(glm::vec3(dt * .2, dt * .2, dt * .2));
+			AA::Engine->GetGameObject(wasd).advanceRotation(glm::vec3(0, dt * .2, 0));
 			});
 
 		AA::Engine->AddToSlowUpdate([]() {
