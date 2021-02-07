c29b33b502696bd43fa5cd671f0ced7d778f4419
Matthew Early <matthewjearly@gmail.com>
2021-02-07 04:20:14 -0500

    rm pre-file comments
    copyright and licenses in root level

--- AAEngine/include/Settings/SettingsOptions.h
+++ AAEngine/include/Settings/SettingsOptions.h
@@ -6,10 +6,6 @@ struct SettingsOptions
 {
 	RenderingFramework renderer = RenderingFramework::OPENGL;
 	WindowingType windowType = WindowingType::WINDOWED;
-	int currWindowWidth = 0;
-	int currWindowHeight = 0;
-	int lastWindowWidth = 0;
-	int lastWindowHeight = 0;
 };
 
 }
