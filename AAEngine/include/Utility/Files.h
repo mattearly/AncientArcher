357111a5f2436f091ec084ef9a079e433a37b72a
Matthew Early <matthewjearly@gmail.com>
2021-02-08 17:19:24 -0500

    add file loader utility

---
+++ AAEngine/include/Utility/Files.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <fstream>
+#include <sstream>
+#include <memory>
+#include <string>
+namespace AA
+{
+class Files
+{
+public:
+	static void ReadToString(const char* filepath, std::string& out) {
+		std::ifstream input_file_stream(filepath);
+		if (!input_file_stream.is_open())
+			throw("invalid file path");
+		std::stringstream data_stream;
+		data_stream << input_file_stream.rdbuf();
+		out = data_stream.str();
+	}
+	static std::string ReadToString(const char* filepath)
+	{
+		std::ifstream input_file_stream(filepath);
+		if (!input_file_stream.is_open())
+			throw("invalid file path");
+		std::stringstream data_stream;
+		data_stream << input_file_stream.rdbuf();
+		return data_stream.str();
+	}
+private:
+};
+}  // end namespace AA
