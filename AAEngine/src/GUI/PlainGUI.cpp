10ccdee8211006a908d123c092d76f662367a417
Matthew Early <matthewjearly@gmail.com>
2021-04-15 02:15:14 -0400

    init plaingui

--- AAEngine/src/GUI/PlainGUI.cpp
+++ AAEngine/src/GUI/PlainGUI.cpp
@@ -1,83 +1,102 @@
 #include "PlainGUI.h"
 #include <vector>
 #include "../Renderer/OpenGL/OGLGraphics.h"
+#include <glad/glad.h>
 namespace AA {
 PlainGUI::PlainGUI() {
   InitShader();
 }
-void PlainGUI::AddSlider(Slider slider, void(*resulthandler)(float result)) {
-  if (slider.bot_left_loc.x < -1.f || slider.bot_left_loc.y < -1.f)
-    throw("beyond -1");
-  if (slider.bot_left_loc.x > 1.f || slider.bot_left_loc.y > 1.f)
-    throw("beyond 1");
 
-  std::vector<vec3> points;
-  points.push_back(vec3(slider.bot_left_loc.x, slider.bot_left_loc.y, 0.f));  //bot left  ind:0
-  if (slider.horizontal) {
-    points.push_back(vec3(slider.bot_left_loc.x,                     slider.bot_left_loc.y + slider.bar_thickness, 0.f));  //top left  1
-    points.push_back(vec3(slider.bot_left_loc.x + slider.bar_length, slider.bot_left_loc.y + slider.bar_thickness, 0.f)); //top right  2
-    points.push_back(vec3(slider.bot_left_loc.x + slider.bar_length, slider.bot_left_loc.y,                        0.f));  // bot right  3
-  } else  //vertical
-  {
-    points.push_back(vec3(slider.bot_left_loc.x,                        slider.bot_left_loc.y + slider.bar_length, 0.f));  //top left  1
-    points.push_back(vec3(slider.bot_left_loc.x + slider.bar_thickness, slider.bot_left_loc.y + slider.bar_length, 0.f));  //top right  2
-    points.push_back(vec3(slider.bot_left_loc.x + slider.bar_thickness, slider.bot_left_loc.y,                     0.f));  // bot right  3
-  }
-
-  std::vector<u32> indices = { 0, 1, 2, 2, 3, 0 };
-
-  vao_id = OGLGraphics::UploadVerts(points, indices);
-  ind_size = sizeof(indices) / sizeof(indices[0]);  // should be 6
+PlainGUI::~PlainGUI() {
+  delete Shader;
+  Shader = NULL;
 }
+
+//void PlainGUI::AddSlider(Slider slider, void(*resulthandler)(float result)) {
+//  if (slider.bot_left_loc.x < -1.f || slider.bot_left_loc.y < -1.f)
+//    throw("beyond -1");
+//  if (slider.bot_left_loc.x > 1.f || slider.bot_left_loc.y > 1.f)
+//    throw("beyond 1");
+//
+//  std::vector<vec3> points;
+//  points.push_back(vec3(slider.bot_left_loc.x, slider.bot_left_loc.y, 0.f));  //bot left  ind:0
+//  if (slider.horizontal) {
+//    points.push_back(vec3(slider.bot_left_loc.x, slider.bot_left_loc.y + slider.bar_thickness, 0.f));  //top left  1
+//    points.push_back(vec3(slider.bot_left_loc.x + slider.bar_length, slider.bot_left_loc.y + slider.bar_thickness, 0.f)); //top right  2
+//    points.push_back(vec3(slider.bot_left_loc.x + slider.bar_length, slider.bot_left_loc.y, 0.f));  // bot right  3
+//  } else  //vertical
+//  {
+//    points.push_back(vec3(slider.bot_left_loc.x, slider.bot_left_loc.y + slider.bar_length, 0.f));  //top left  1
+//    points.push_back(vec3(slider.bot_left_loc.x + slider.bar_thickness, slider.bot_left_loc.y + slider.bar_length, 0.f));  //top right  2
+//    points.push_back(vec3(slider.bot_left_loc.x + slider.bar_thickness, slider.bot_left_loc.y, 0.f));  // bot right  3
+//  }
+//
+//  std::vector<u32> indices = { 0, 1, 2, 2, 3, 0 };
+//
+//  vao_id = OGLGraphics::UploadVerts(points, indices);
+//  ind_size = sizeof(indices) / sizeof(indices[0]);  // should be 6
+//}
 
 void PlainGUI::InitShader() {
   if (Shader)
     return;
 
+  std::vector<vec2> square_points = {
+    vec2(-1, 1),
+    vec2(-1, -1),
+    vec2(1, 1),
+    vec2(1, -1),
+  };
+
+  vao_id = OGLGraphics::Upload2DVerts(square_points);
+
   const char* vert =
     "#version 430 core\n"
-    "layout(location = 0) in vec3 aPos;\n"
-    //"uniform vec3 color;\n";
-    //"out pass_color;\n"
+    "layout(location = 0) in vec2 aPos;\n"
+    //"out vec2 textureCoords;\n"
+    //"uniform mat4 transformationMatrix;\n"
     "void main(){\n"
-    //"  pass_color = color;\n"
-    "  vec4 pos = vec4(aPos, 1.0);\n"
-    "  gl_Position = pos.xywz;\n"
+    "  gl_Position = vec4(aPos, 0.0, 1.0);\n"
+    //"  textureCoords = vec2((aPos.x+1.0)/2.0, 1-(aPos.y+1.0)/2.0);\n"
     "}\n"
     ;
   const char* frag =
     "#version 430 core\n"
-    //"in vec3 pass_color;\n"
-    "uniform vec2 u_resolution;\n"
-    "uniform vec2 u_mouse;\n"
-    "uniform float u_time;\n"
-    "float plot (vec2 st) {\n"
-    "  return smoothstep(0.02, 0.0, abs(st.y - st.x));\n"
-    "}\n"
-    //"out vec4 FragColor;\n"
+    //"in vec2 textureCoords;\n"
+    "out vec4  out_Color;\n"
+    //"uniform sampler2D guiTexture;\n"
     "void main() {\n"
-    "  vec2 st = gl_FragCoord.xy / u_resolution;\n"
-    "  vec3 color = vec3(y);\n"
-    "  float pct = plot(st);\n"
-    "  color = (1.0-pct)*color+pct*vec3(0.0,1.0,0.0);\n"
-    "  gl_FragColor = vec4(color,1.0);\n"
+    //"  out_Color = texture(guiTexture, textureCoords);"
+    "  out_Color = vec4(.5,.5,.5,1.0);\n"
     "}\n"
     ;
   Shader = new OGLShader(vert, frag);
 
+  total_time = 0;
 }
 
 void PlainGUI::Draw() {
-  glDepthFunc(GL_LEQUAL);  // change depth function so depth test passes when values are equal to depth buffer's content
+  //glDepthFunc(GL_LEQUAL);  // change depth function so depth test passes when values are equal to depth buffer's content
+  //glEnable(GL_DEPTH_TEST);
 
-  glBindVertexArray(mSkyboxVAO);
-
-  glActiveTexture(GL_TEXTURE0);
-  glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
-  skyboxShader->use();
-  glDrawArrays(GL_TRIANGLES, 0, 36);
+  Shader->use();
+  glBindVertexArray(vao_id);
+  glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
   glBindVertexArray(0);
-  glDepthFunc(GL_LESS); // set depth function back to default
+
+  //glDepthFunc(GL_LESS);  // set depth function back to default
+}
+
+void PlainGUI::UpdateUniforms(vec2 resolution, vec2 mouse_loc, float time){
+  if (!Shader)
+    return;
+
+  total_time+=time;
+  Shader->use();
+  //Shader->setVec2("u_resolution", resolution);
+  //Shader->setVec2("u_mouse", mouse_loc);
+  //Shader->setFloat("u_time", total_time);
+
 }
 
 }
--- AAEngine/src/GUI/PlainGUI.h
+++ AAEngine/src/GUI/PlainGUI.h
@@ -1,26 +1,35 @@
 #pragma once
-#include "../Base/Types.h"
+#include "../../include/AncientArcher/Types.h"
 #include "../Renderer/OpenGL/OGLShader.h"
-namespace AA{
+#include <vector>
+namespace AA {
+
 struct Slider {
   vec2 bot_left_loc;
-  float low_val; 
-  float high_val;
-  float bar_thickness;
-  float bar_length;
+  f32 low_val;
+  f32 high_val;
+  f32 bar_thickness;
+  f32 bar_length;
   bool horizontal;
-  };
+};
+
 class PlainGUI {
 public:
   PlainGUI();
-  void AddSlider(Slider slider, void(*resulthandler)(float result));
+  ~PlainGUI();
+  //void AddSlider(Slider slider, void(*resulthandler)(float result));
+
   void InitShader();
-  static OGLShader* Shader;
+  void UpdateUniforms(vec2 resolution, vec2 mouse_loc, float time);
+  OGLShader* Shader;
   void Draw();
   int vao_id;
   int ind_size;
+
 private:
   std::vector<Slider> p_Sliders;
+
+  f32 total_time = 0;
 };
 
 }
