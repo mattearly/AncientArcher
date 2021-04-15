0c824c1f1c88ceeba2d0b69f4801d6bdc04590d0
Matthew Early <matthewjearly@gmail.com>
2021-04-15 02:12:29 -0400

    update model matrix if flagged

--- AAEngine/src/Scene/Prop.cpp
+++ AAEngine/src/Scene/Prop.cpp
@@ -4,49 +4,37 @@
 #include "../Renderer/ModelLoader.h"
 #include <iostream>
 
-namespace AA
-{
-const int Prop::GetCamId() const noexcept
-{
+namespace AA {
+const int Prop::GetCamId() const noexcept {
   return mCameraID;
 }
 
-Prop::Prop(const char* path, int camId, SHADERTYPE shadertype)
-{
+Prop::Prop(const char* path, int camId, SHADERTYPE shadertype) {
   ModelLoader::LoadGameObjectFromFile(mMeshes, path);
   mCameraID = camId;
   mShaderType = shadertype;
 }
 
-void Prop::SetCamera(int id) noexcept
-{
+void Prop::SetCamera(int id) noexcept {
   mCameraID = id;
 }
 
-void Prop::draw()
-{
+void Prop::draw() {
   if (mCameraID == -1)
     throw("camera not set for this prop");
-
+  if (mModelMatrixChanged) {
+    updateFinalModelMatrix();
+    mModelMatrixChanged = false;
+  }
   OGLGraphics::Render(mMeshes, finalModelMatrix, mShaderType);
 }
 
-void Prop::updateFinalModelMatrix()
-{
+void Prop::updateFinalModelMatrix() {
   // reset
   finalModelMatrix = glm::mat4(1);
   finalModelMatrix = glm::translate(finalModelMatrix, translation);
   // apply current scale
   finalModelMatrix = glm::scale(finalModelMatrix, scale);
-
-  // apply current rotation  (x only)
-  if (eulerRotationX == 0.f)
-    return;
-  if (eulerRotationX < 0.f)
-    eulerRotationX += 360.f;
-  if (eulerRotationX > 360.f)
-    eulerRotationX -= 360.f;
-  finalModelMatrix = glm::rotate(finalModelMatrix, eulerRotationX, glm::vec3(1, 0, 0));
 
   // apply current rotation  (y only)
   if (eulerRotationY == 0.f)
@@ -56,21 +44,6 @@ void Prop::updateFinalModelMatrix()
   if (eulerRotationY > 360.f)
     eulerRotationY -= 360.f;
   finalModelMatrix = glm::rotate(finalModelMatrix, eulerRotationY, glm::vec3(0, 1, 0));
-
-  //// apply current rotation  (z only)
-  //if (eulerRotationZ == 0.f)
-  //  return;
-  //if (eulerRotationZ < 0.f)
-  //  eulerRotationZ += 360.f;
-  //if (eulerRotationZ > 360.f)
-  //  eulerRotationZ -= 360.f;
-  //finalModelMatrix = glm::rotate(finalModelMatrix, eulerRotationZ, glm::vec3(0,0,1));
-
-  //finalModelMatrix *= glm::rotate(finalModelMatrix, eulerRotationX, glm::vec3(1, 0, 0));
-  //finalModelMatrix *= glm::rotate(finalModelMatrix, eulerRotationY, glm::vec3(0, 1, 0));
-  //finalModelMatrix *= glm::rotate(finalModelMatrix, eulerRotationZ, glm::vec3(0, 0, 1));
-
-  //glm::quat rotation =  glm::toQuat(eulerRot);
 
 }
 
--- AAEngine/src/Scene/Prop.h
+++ AAEngine/src/Scene/Prop.h
@@ -11,51 +11,14 @@
 #include "../Renderer/MeshDrawInfo.h"
 #include "ColliderSphere.h"
 #include "../../include/AncientArcher/ShaderType.h"
-namespace AA
-{
-class Prop : public UniqueInstance
-{
+namespace AA {
+class Prop : public UniqueInstance {
 public:
 
   Prop(const char* path, int camId, SHADERTYPE shadertype);
   //Prop(const char* path, int camId, bool lit, std::vector<InstanceDetails> details);
   const int GetCamId() const noexcept;
   void SetCamera(int id) noexcept;
-
-  // getters
-  //const glm::vec3& GetLocation() const;
-  //const glm::vec3& GetLocation(int which) const;
-  //const glm::vec3& GetRotation() const;
-  //const glm::vec3& GetRotation(int which) const;
-  //int GetModelMatrix(const int& which, glm::mat4& out_mat4) const;
-  //glm::mat4 GetModelMatrix(const int& which);
-  //const int GetShaderId() const noexcept;
-
-  //const std::size_t GetInstanceCount() const noexcept;
-  //const ColliderSphere* GetColliderSphere(uint32_t which = 0) const;
-  //bool IsSingleInstance() const;
-
-  // setters
-
-  //void SetShader(int id) noexcept;
-  //void SetColliderSphere(const glm::vec3& center, const float& radius, uint32_t which = 0, bool overwrite = true) noexcept;
-
-  //void SetScale(glm::vec3 amt, int which);
-  //void SetScale(glm::vec3 amt);
-  //void SetRotation(glm::vec3 new_rot, int which);
-  //void SetRotation(glm::vec3 new_rot);
-  //void SetTranslation(glm::vec3 amt, int which);
-  //void SetTranslation(glm::vec3 amt);
-
-  //void AddToScale(glm::vec3 amt);
-  //void AddToScale(glm::vec3 amt, int which);
-  //void AddToRotation(glm::vec3 radianAmt);
-  //void AddToRotation(glm::vec3 radianAmt, int which);
-  //void AddToTranslation(glm::vec3 amt);
-  //void AddToTranslation(glm::vec3 amt, int which);
-
-  //void AddInstance(const InstanceDetails& instance_details);
-  //bool RemoveInstance(int which);
 
   void draw();
   SHADERTYPE mShaderType;
@@ -66,23 +29,19 @@ public:
 
   //glm::quat rotation = glm::quat();
   //glm::vec3 rotationAngles = glm::vec3(0);
-  float eulerRotationX = 0.f;
+  //float eulerRotationX = 0.f;
   float eulerRotationY = 0.f;  // direction
-  float eulerRotationZ = 0.f;
+  //float eulerRotationZ = 0.f;
 
   glm::vec3 scale = glm::vec3(1);
 
   glm::mat4 finalModelMatrix = glm::mat4(1);
 
-  void updateFinalModelMatrix();
-
+  bool mModelMatrixChanged = false;
 private:
 
+  void updateFinalModelMatrix();
   int mCameraID = -1;
 
-  //std::vector<InstanceDetails> mInstanceDetails;  // for instancing multiple objects, sized to the number of this object in our world
-
-  // helpers
-  //void calculateNewModelMatrix(int which);
 };
 }  // end namespace AA
