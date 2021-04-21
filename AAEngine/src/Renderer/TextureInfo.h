1a43624c608ace0430c2510ab29be4aeab4dad68
Matthew Early <matthewjearly@gmail.com>
2021-04-21 03:24:27 -0400

    default assign

--- AAEngine/src/Renderer/TextureInfo.h
+++ AAEngine/src/Renderer/TextureInfo.h
@@ -2,11 +2,10 @@
 #include <string>
 namespace AA
 {
-struct TextureInfo
-{
+struct TextureInfo {
   uint32_t accessId = 0; // id to access it on the video mem (drawId)
-  std::string type;
-  std::string path;
+  std::string type = "";
+  std::string path = "";
   int ref_count = 1;
 };
 }
