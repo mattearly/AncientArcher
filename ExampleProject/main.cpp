867d866a7bf86ae9cc2370330935baa4f83f785a
Matthew Early <matthewjearly@gmail.com>
2021-05-15 18:39:01 -0400

    formatting, update initial cout

--- ExampleProject/main.cpp
+++ ExampleProject/main.cpp
@@ -1,5 +1,6 @@
 #include <AncientArcher/AncientArcher.h>
 #include <AncientArcher/Version.h>
+#include <AncientArcher/Utility/Files.h>
 #include <iostream>
 #include "setskybox.h"
 #include "setfpsplayercontrols.h"
@@ -9,25 +10,25 @@
 #include "setdirlight.h"
 #include "setmodels.h"
 
-int main() {
-  std::cout << "Running Version: " << AA::ENGINEVERSIONMAJOR << '.' << AA::ENGINEVERSIONMINOR << '.' << AA::ENGINEVERSIONPATCH << '\n';
-  std::cout << "EXAMPLE PROJECT: " << "sample code for above version\n"
-            << "Hotkeys:\n" 
-            << "    -" << "WASD:  Move\n"
-            << "    -" << "Mouse: Look\n"
-            << "    -" << "TAB:   Toggle Mouse Control (Inventory GUI)\n"
-            << "    -" << "F:     Toggle Flashlight\n"
-            << "    -" << "1:     Sound Effect (lazer sound)\n"
-            << "    -" << "2:     Sound Effect (enchant sound)\n"
-            << "    -" << "F1:    Toggle Sky Light\n"
-            << "    -" << "P:     Play Long Sound (Music)\n"
-            << "    -" << "O:     Stop Long Sound (Music)\n"
-            << "    -" << "I:     Pause Long Sound (Music)\n"
-            << "    -" << "K:     Resume Long Sound (Music)\n"
-            << "    -" << "U:     Unload Long Sound (Music)\n"
-            << "    -" << "R:     Reload Long Sound (Music)\n"
-            << "    -" << "Wheel: Vol Up/Down (Music)\n"
-            ;
+int main(int argc, char** argv) {
+  std::cout << "AncientArcherEngineVersion: " << AA::ENGINEVERSIONMAJOR << '.' << AA::ENGINEVERSIONMINOR << '.' << AA::ENGINEVERSIONPATCH << '\n';
+  std::cout << "Running: " << AA::Files::extract_prog_name(argv[0]) << '\n'
+    << "Hotkeys:\n"
+    << " WASD:  Move\n"
+    << " Mouse: Look\n"
+    << " TAB:   Toggle Mouse Control (Inventory GUI)\n"
+    << " F:     Toggle Flashlight\n"
+    << " 1:     Sound Effect (lazer sound)\n"
+    << " 2:     Sound Effect (enchant sound)\n"
+    << " F1:    Toggle Sky Light\n"
+    << " P:     Play Long Sound (Music)\n"
+    << " O:     Stop Long Sound (Music)\n"
+    << " I:     Pause Long Sound (Music)\n"
+    << " K:     Resume Long Sound (Music)\n"
+    << " U:     Unload Long Sound (Music)\n"
+    << " R:     Reload Long Sound (Music)\n"
+    << " Wheel: Vol Up/Down (Music)\n"
+    ;
   AA::InitEngine();
   static int ourcam = AA::AddCamera(AA::GetWindowWidth(), AA::GetWindowHeight());
   setfpsplayercontrols(ourcam);
@@ -37,7 +38,6 @@ int main() {
   setupsoundeffects();
   setmusic();
   setmodels(ourcam);
-
   AA::SetWindowClearColor(glm::vec3(0));
   return AA::Run();
 }
