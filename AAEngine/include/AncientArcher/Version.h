f32e89aca4fd7aa52b8b3c5a766c4203b6799841
Matthew Early <matthewjearly@gmail.com>
2021-07-03 14:18:01 -0400

    inc version to 005

--- AAEngine/include/AncientArcher/Version.h
+++ AAEngine/include/AncientArcher/Version.h
@@ -2,5 +2,5 @@
 namespace AA {
 constexpr unsigned int ENGINEVERSIONMAJOR = 0;
 constexpr unsigned int ENGINEVERSIONMINOR = 0;
-constexpr unsigned int ENGINEVERSIONPATCH = 4;
+constexpr unsigned int ENGINEVERSIONPATCH = 5;
 }
